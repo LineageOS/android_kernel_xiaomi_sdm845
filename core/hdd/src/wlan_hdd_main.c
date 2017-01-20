f8ed998d7f162ef717ab186de02a588672195f3f
Jeff Johnson <jjohnson@codeaurora.org>
2017-01-20 07:18:27 -0800

    qcacld-3.0: Invoke HDD Request Manager init/deinit
    Enable the HDD Request Manager by invoking the init() and deinit()
    APIs as appropriate.
    
    Change-Id: I31e268ca02b4b5c2831c540933ee059a27bd9c7e
    CRs-Fixed: 2005301

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -54,6 +54,7 @@
 #include "wlan_hdd_power.h"
 #include "wlan_hdd_stats.h"
 #include "wlan_hdd_scan.h"
+#include "wlan_hdd_request_manager.h"
 #include "qdf_types.h"
 #include "qdf_trace.h"
 #include <cdp_txrx_peer_ops.h>
@@ -6034,6 +6035,7 @@ static void hdd_wlan_exit(hdd_context_t *hdd_ctx)
 	 */
 
 	hdd_green_ap_deinit(hdd_ctx);
+	hdd_request_manager_deinit();
 
 	hdd_runtime_suspend_context_deinit(hdd_ctx);
 	hdd_close_all_adapters(hdd_ctx, false);
@@ -10229,6 +10231,7 @@ int hdd_wlan_startup(struct device *dev)
 	if (ret)
 		goto err_hdd_free_context;
 
+	hdd_request_manager_init();
 	hdd_green_ap_init(hdd_ctx);
 
 	hdd_init_spectral_scan(hdd_ctx);
@@ -10355,6 +10358,7 @@ err_exit_nl_srv:
 	qdf_mc_timer_destroy(&hdd_ctx->tdls_source_timer);
 
 	hdd_green_ap_deinit(hdd_ctx);
+	hdd_request_manager_deinit();
 	hdd_exit_netlink_services(hdd_ctx);
 
 err_hdd_free_context:
