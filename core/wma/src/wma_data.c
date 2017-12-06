3a2dc1d55eb7e0de3048fbb40cb035e0fef5a34f
Kiran Kumar Lokere <klokere@codeaurora.org>
2017-12-05 18:40:52 -0800

    qcacld-3.0: Fix the resource leak when Tx complete fails
    Fix the resource leakage when Tx completion event is not
    received from FW for the Tx management frame
    
    Change-Id: I17eebfeadf6d6be8d41c61c9a1687df57880f9de
    CRs-Fixed: 2154060

--- core/wma/src/wma_data.c
+++ core/wma/src/wma_data.c
@@ -2431,14 +2431,23 @@ void wmi_desc_pool_deinit(tp_wma_handle wma_handle)
 {
 	struct wmi_desc_t *wmi_desc;
 	uint8_t i;
+	qdf_device_t qdf_dev = cds_get_context(QDF_MODULE_ID_QDF_DEVICE);
 
+	if (NULL == qdf_dev) {
+		WMA_LOGE("%s: Failed to get qdf_dev_ctx", __func__);
+		return;
+	}
 	qdf_spin_lock_bh(&wma_handle->wmi_desc_pool.wmi_desc_pool_lock);
 	if (wma_handle->wmi_desc_pool.array) {
 		for (i = 0; i < wma_handle->wmi_desc_pool.pool_size; i++) {
 			wmi_desc = (struct wmi_desc_t *)
 				    (&wma_handle->wmi_desc_pool.array[i]);
-			if (wmi_desc && wmi_desc->nbuf)
+			if (wmi_desc && wmi_desc->nbuf) {
+				qdf_nbuf_unmap_single(qdf_dev,
+						wmi_desc->nbuf,
+						QDF_DMA_TO_DEVICE);
 				cds_packet_free(wmi_desc->nbuf);
+			}
 		}
 		qdf_mem_free(wma_handle->wmi_desc_pool.array);
 		wma_handle->wmi_desc_pool.array = NULL;
