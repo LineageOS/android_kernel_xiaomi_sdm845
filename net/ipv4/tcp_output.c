d38af947be6a2b3d33cbc16af697d7fb221e6b15
Neal Cardwell <ncardwell@google.com>
2017-12-11 15:42:53 -0800

    tcp: allow TLP in ECN CWR
    This patch enables tail loss probe in cwnd reduction (CWR) state
    to detect potential losses. Prior to this patch, since the sender
    uses PRR to determine the cwnd in CWR state, the combination of
    CWR+PRR plus tcp_tso_should_defer() could cause unnecessary stalls
    upon losses: PRR makes cwnd so gentle that tcp_tso_should_defer()
    defers sending wait for more ACKs. The ACKs may not come due to
    packet losses.
    
    Disallowing TLP when there is unused cwnd had the primary effect
    of disallowing TLP when there is TSO deferral, Nagle deferral,
    or we hit the rwin limit. Because basically every application
    write() or incoming ACK will cause us to run tcp_write_xmit()
    to see if we can send more, and then if we sent something we call
    tcp_schedule_loss_probe() to see if we should schedule a TLP. At
    that point, there are a few common reasons why some cwnd budget
    could still be unused:
    
    (a) rwin limit
    (b) nagle check
    (c) TSO deferral
    (d) TSQ
    
    For (d), after the next packet tx completion the TSQ mechanism
    will allow us to send more packets, so we don't really need a
    TLP (in practice it shouldn't matter whether we schedule one
    or not). But for (a), (b), (c) the sender won't send any more
    packets until it gets another ACK. But if the whole flight was
    lost, or all the ACKs were lost, then we won't get any more ACKs,
    and ideally we should schedule and send a TLP to get more feedback.
    In particular for a long time we have wanted some kind of timer for
    TSO deferral, and at least this would give us some kind of timer
    
    Reported-by: Steve Ibanez <sibanez@stanford.edu>
    Change-Id: Iea00ddb2c6861f23044abbf78e3e45a5b14380e6
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: Yuchung Cheng <ycheng@google.com>
    Reviewed-by: Nandita Dukkipati <nanditad@google.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: awkoo <awkooox@gmail.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2370,14 +2370,12 @@ bool tcp_schedule_loss_probe(struct sock *sk)
 		return false;
 
 	/* Schedule a loss probe in 2*RTT for SACK capable connections
-	 * in Open state, that are either limited by cwnd or application.
+	 * not in loss recovery, that are either limited by cwnd or application.
 	 */
 	if (sysctl_tcp_early_retrans < 3 || !tp->packets_out ||
-	    !tcp_is_sack(tp) || inet_csk(sk)->icsk_ca_state != TCP_CA_Open)
-		return false;
-
-	if ((tp->snd_cwnd > tcp_packets_in_flight(tp)) &&
-	     tcp_send_head(sk))
+	    !tcp_is_sack(tp) ||
+	    (inet_csk(sk)->icsk_ca_state != TCP_CA_Open &&
+	     inet_csk(sk)->icsk_ca_state != TCP_CA_CWR))
 		return false;
 
 	/* Probe timeout is at least 1.5*rtt + TCP_DELACK_MAX to account
