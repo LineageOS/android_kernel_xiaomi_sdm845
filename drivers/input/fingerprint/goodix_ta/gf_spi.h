633c0ad86bc08babd33ce41daa1f7af6a0c4995b
Sam Mortimer <sam@mortimer.me.uk>
2018-01-05 15:01:15 -0800

    input: fingerprint: goodix: Add proximity_state sysfs support
    *) Implements sysfs proximity_state so that fp reader
       can be disabled by PocketMode app.
    
    Change-Id: If0e6fa3172e1546d989de8522b2bdd734a3840a6

--- drivers/input/fingerprint/goodix_ta/gf_spi.h
+++ drivers/input/fingerprint/goodix_ta/gf_spi.h
@@ -143,6 +143,7 @@ struct gf_dev {
 #ifdef CONFIG_FINGERPRINT_FP_VREG_CONTROL
 	struct regulator *vreg;
 #endif
+	int proximity_state; /* 0:far 1:near */
 };
 
 int gf_parse_dts(struct gf_dev *gf_dev);
