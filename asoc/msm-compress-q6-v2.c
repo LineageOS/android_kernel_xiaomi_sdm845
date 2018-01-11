40f12144c094da318310da8ad7cde5b0098e5629
Vikram Panduranga <vpandura@codeaurora.org>
2018-01-10 18:04:54 -0800

    asoc: Fix read offset for compress capture with timestamp
    In a compress capture usecase, read offset guides to
    move buffer pointer based on buffer length. When timestamp is
    enabled read offset also needs to include timestamp header
    offset along with buffer length. This fix addresses the issue
    of null memory map handle due to incorrect read offset.
    
    Change-Id: I93c3ec588e77d535b6c7a4a0d832c1e7ea5f62a7
    Signed-off-by: Vikram Panduranga <vpandura@codeaurora.org>

--- asoc/msm-compress-q6-v2.c
+++ asoc/msm-compress-q6-v2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -541,7 +541,7 @@ static int msm_compr_read_buffer(struct msm_compr_audio *prtd)
 		return ret;
 	}
 	prtd->bytes_read += buffer_length;
-	prtd->bytes_read_offset += buffer_length;
+	prtd->bytes_read_offset += buffer_length + prtd->ts_header_offset;
 	if (prtd->bytes_read_offset >= prtd->buffer_size)
 		prtd->bytes_read_offset -= prtd->buffer_size;
 
