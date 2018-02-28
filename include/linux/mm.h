88afbb0345afddaa9772525b394c22b8550ed90f
Wei Wang <wvw@google.com>
2018-02-28 13:34:22 -0800

    ANDROID: mm: add config for default readahead size
    Change the VM_MAX_READAHEAD value from the default 128KB to a
    configurable value. This will allow the readahead window to grow to a
    maximum size bigger than 128KB, which greatly benefits to sequential
    read throughput and thus boot performance.
    
    Bug: 62413151
    Test: boot walleye 100ms faster
    Change-Id: Iad448cf1198056de46654dcb409466802b3b908d
    Signed-off-by: Wei Wang <wvw@google.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2224,7 +2224,7 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	512	/* kbytes */
+#define VM_MAX_READAHEAD	CONFIG_VM_MAX_READAHEAD	/* kbytes */
 #define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
