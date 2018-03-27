ac7ac81b772bcd10f174ff5035542248edcdb071
Himanshu Agarwal <himanaga@codeaurora.org>
2018-03-27 15:36:09 +0530

    qcacld-3.0: Release WM status change cmd after eWNI_SME_DISCONNECT_DONE_IND
    When deauth is received from AP, while processing of deauth frame,
    WM status change command is queued in SME command pending list with
    priority set as true in which DEL_BSS and DEL_STA happens leading to
    VDEV_STOP AND VDEV_DOWN correspondingly.
    
    When disconnect is issued from upper layer, ROAM command with reason
    eCsrForcedDisassoc gets queued in SME command pending list with priority
    set as true which performs DEL_BSS nad DEL_STA and then
    eSmeCommandDelStaSession SME command is queued with priority set as false
    which performs DEL_SELF_STA.
    
    If disconnect is issued from upper layer and deauth is received from AP at
    the same time, it might happen that ROAM SME command and
    eSmeCommandDelStaSession SME command gets queued in SME command pending
    list but WM status change command gets queued on top of these as priority
    is set to true before the former commands can be processed. While
    processiing of WM status change command, eWNI_SME_DEAUTH_CNF msg gets
    queued in SME message queue which queues WMA_DELETE_BSS_REQ in WMA msg
    queue. If WM staus change command is released just after
    eWNI_SME_DEAUTH_CNF is posted, it might happen that Roam and
    eSmeCommandDelStaSession SME commands from SME command pending list gets
    processed first which will queue WMA_DEL_STA_SELF_REQ in WMA msg queue
    before eWNI_SME_DEAUTH_CNF gets processed and queue WMA_DELETE_BSS_REQ
    in WMA msg queue. This leads to processing of WMA_DEL_STA_SELF_REQ before
    WMA_DELETE_BSS_REQ causing assert as this is unexpected behaviour.
    
    Release WM status change command only after eWNI_SME_DISCONNECT_DONE_IND
    which happens after WMA_DELETE_BSS_REQ and WMA_DELETE_STA_REQ gets
    processed so that ROAM and eSmeCommandDelStaSession SME commands gets to
    process only after processing of DEL_BSS and DEL_STA and so
    WMA_DEL_STA_SELF_REQ will always be processed after WMA_DELETE_BSS_REQ
    avoidong system assert.
    
    Change-Id: Iec0176fecf218e07f31b258c0dc52aefb480defe
    CRs-Fixed: 2211622

--- core/sme/src/csr/csr_api_roam.c
+++ core/sme/src/csr/csr_api_roam.c
@@ -11403,6 +11403,12 @@ csr_roam_send_disconnect_done_indication(tpAniSirGlobal mac_ctx, tSirSmeRsp
 	} else
 		sme_err("Inactive session %d",
 			discon_ind->session_id);
+
+	/*
+	 * Release WM status change command as eWNI_SME_DISCONNECT_DONE_IND
+	 * has been sent to HDD and there is nothing else left to do.
+	 */
+	csr_roam_wm_status_change_complete(mac_ctx);
 }
 
 static void
@@ -12751,7 +12757,7 @@ QDF_STATUS csr_roam_lost_link(tpAniSirGlobal pMac, uint32_t sessionId,
 }
 
 
-static void csr_roam_wm_status_change_complete(tpAniSirGlobal pMac)
+void csr_roam_wm_status_change_complete(tpAniSirGlobal pMac)
 {
 	tListElem *pEntry;
 	tSmeCmd *pCommand;
@@ -12786,7 +12792,7 @@ void csr_roam_process_wm_status_change_command(tpAniSirGlobal pMac,
 
 	if (!pSession) {
 		sme_err("session %d not found", pCommand->sessionId);
-		return;
+		goto end;
 	}
 	sme_debug("session:%d, CmdType : %d",
 		pCommand->sessionId, pCommand->u.wmStatusChangeCmd.Type);
@@ -12813,10 +12819,15 @@ void csr_roam_process_wm_status_change_command(tpAniSirGlobal pMac,
 			pCommand->u.wmStatusChangeCmd.Type);
 		break;
 	}
-	/* Lost Link just triggers a roaming sequence.  We can complte the
-	 * Lost Link command here since there is nothing else to do.
-	 */
-	csr_roam_wm_status_change_complete(pMac);
+
+end:
+	if (status != QDF_STATUS_SUCCESS) {
+		/*
+		 * As status returned is not success, there is nothing else
+		 * left to do so release WM status change command here.
+		 */
+		csr_roam_wm_status_change_complete(pMac);
+	}
 }
 
 
