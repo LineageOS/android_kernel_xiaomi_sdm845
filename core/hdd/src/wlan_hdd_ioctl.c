c456323b49c30fa97d46d887aae887a7dd0e0563
Srinivas Girigowda <sgirigow@codeaurora.org>
2018-04-17 14:42:23 -0700

    qcacld-3.0: Add dummy function for supplicant deprecated commands
    Fix the VTS test failures when running the following
    deprecated commands in SupplicantStaIfaceHidlTest
      RXFILTER-START
      RXFILTER-STOP
      BTCOEXSCAN-START
      BTCOEXSCAN-STOP
    
    Change-Id: I45fa09c24700e6872de7709c6875dbdbd8aa10cc
    CRs-Fixed: 2226343

--- core/hdd/src/wlan_hdd_ioctl.c
+++ core/hdd/src/wlan_hdd_ioctl.c
@@ -6923,8 +6923,6 @@ static const struct hdd_drv_cmd hdd_drv_cmds[] = {
 	{"COUNTRY",                   drv_cmd_country, true},
 	{"SETSUSPENDMODE",            drv_cmd_dummy, false},
 	{"SET_AP_WPS_P2P_IE",         drv_cmd_dummy, false},
-	{"BTCOEXSCAN",                drv_cmd_dummy, false},
-	{"RXFILTER",                  drv_cmd_dummy, false},
 	{"SETROAMTRIGGER",            drv_cmd_set_roam_trigger, true},
 	{"GETROAMTRIGGER",            drv_cmd_get_roam_trigger, false},
 	{"SETROAMSCANPERIOD",         drv_cmd_set_roam_scan_period, true},
@@ -7020,7 +7018,12 @@ static const struct hdd_drv_cmd hdd_drv_cmds[] = {
 	{"CHANNEL_SWITCH",            drv_cmd_set_channel_switch, true},
 	{"SETANTENNAMODE",            drv_cmd_set_antenna_mode, true},
 	{"GETANTENNAMODE",            drv_cmd_get_antenna_mode, false},
+	/* Deprecated commands */
 	{"STOP",                      drv_cmd_dummy, false},
+	{"RXFILTER-START",            drv_cmd_dummy, false},
+	{"RXFILTER-STOP",             drv_cmd_dummy, false},
+	{"BTCOEXSCAN-START",          drv_cmd_dummy, false},
+	{"BTCOEXSCAN-STOP",           drv_cmd_dummy, false},
 };
 
 /**
