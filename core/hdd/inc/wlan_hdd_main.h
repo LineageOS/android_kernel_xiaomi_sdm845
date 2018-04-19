58c4cc1c5b8ed744df5a41d0866db70c8ab60752
Varun Reddy Yeturu <varunreddy.yeturu@codeaurora.org>
2018-04-18 19:10:34 -0700

    qcacld-3.0: Process TWT enable complete event
    Process the TWT enable complete event sent by the
    firmware after enabling TWT.
    Set the appropriate state for TWT so that it can
    be used later to check if TWT is enabled or not
    in the target.
    
    Change-Id: I924387d6afc2bf80efec0fce36ea907c6932dcda
    CRs-Fixed: 2238302

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -92,6 +92,7 @@
 #include <net/neighbour.h>
 #include <net/netevent.h>
 #include "wlan_hdd_nud_tracking.h"
+#include "wlan_hdd_twt.h"
 
 /*
  * Preprocessor definitions and constants
@@ -1902,6 +1903,9 @@ struct hdd_context {
 	/* defining the board related information */
 	uint32_t hw_bd_id;
 	struct board_info hw_bd_info;
+#ifdef WLAN_FEATURE_TWT
+	enum twt_status twt_state;
+#endif
 };
 
 /**
