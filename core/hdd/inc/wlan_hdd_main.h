1a5a1f06eec959665fb442ec36196c8a016a97ab
Dundi Raviteja <dundi@codeaurora.org>
2018-04-25 18:13:36 +0530

    qcacld-3.0: Use request manager for linkspeed
    We are transitioning to the new request manager framework.
    
    Change wlan_hdd_get_linkspeed_for_peermac() and hdd_get_link_speed_cb() to
    this framework. Note that this framework provides the infrastructure
    to pass data from the response thread to the request thread and hence
    eliminates the need to maintain tSirLinkSpeedInfo in the HDD adapter
    struct.
    
    Change-Id: I27cdbb986b23c4895f7e6e444697994f26bc6ea1
    CRs-Fixed: 2005313

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -412,12 +412,6 @@ struct statsContext {
 	unsigned int magic;
 };
 
-struct linkspeedContext {
-	struct completion completion;
-	hdd_adapter_t *pAdapter;
-	unsigned int magic;
-};
-
 extern spinlock_t hdd_context_lock;
 extern struct mutex hdd_init_deinit_lock;
 
@@ -425,7 +419,6 @@ extern struct mutex hdd_init_deinit_lock;
 #define PEER_INFO_CONTEXT_MAGIC 0x50494E46  /* PEER_INFO(PINF) */
 #define POWER_CONTEXT_MAGIC 0x504F5752  /* POWR */
 #define SNR_CONTEXT_MAGIC   0x534E5200  /* SNR */
-#define LINK_CONTEXT_MAGIC  0x4C494E4B  /* LINKSPEED */
 #define LINK_STATUS_MAGIC   0x4C4B5354  /* LINKSTATUS(LNST) */
 #define TEMP_CONTEXT_MAGIC  0x74656d70   /* TEMP (temperature) */
 #define BPF_CONTEXT_MAGIC 0x4575354    /* BPF */
@@ -1321,8 +1314,9 @@ struct hdd_adapter_s {
 	struct net_device_stats stats;
 	/** HDD statistics*/
 	hdd_stats_t hdd_stats;
-	/** linkspeed statistics */
-	tSirLinkSpeedInfo ls_stats;
+
+	/* estimated link speed */
+	u32 estimated_linkspeed;
 	/* SAP peer station info */
 	struct sir_peer_sta_info peer_sta_info;
 
