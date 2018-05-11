56486ca44f19ebf01a53dbf8deb6ab9249827b57
Zhu Jianmin <jianminz@codeaurora.org>
2018-05-11 10:54:10 +0800

    qcacld-3.0: ini parameter to allow STA+SAP SCC on LTE coex channel
    When Force SCC and STA+SAP SCC on LTE coex channel are enabled:
       1. When STA on LTE coex channel, start SAP, select STA
          channel.
       2. When SAP on, connect STA on LTE coex channel, then switch
          SAP channel to STA channel.
    
    Change-Id: I3f3972df43318473342d42012be3a57b8baad965
    CRs-Fixed: 2235704

--- core/hdd/src/wlan_hdd_cfg.c
+++ core/hdd/src/wlan_hdd_cfg.c
@@ -3600,6 +3600,12 @@ struct reg_table_entry g_registry_table[] = {
 		     CFG_STA_SAP_SCC_ON_DFS_CHAN_MIN,
 		     CFG_STA_SAP_SCC_ON_DFS_CHAN_MAX),
 
+	REG_VARIABLE(CFG_STA_SAP_SCC_ON_LTE_COEX_CHAN, WLAN_PARAM_HexInteger,
+		     struct hdd_config, sta_sap_scc_on_lte_coex_chan,
+		     VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+		     CFG_STA_SAP_SCC_ON_LTE_COEX_CHAN_DEFAULT,
+		     CFG_STA_SAP_SCC_ON_LTE_COEX_CHAN_MIN,
+		     CFG_STA_SAP_SCC_ON_LTE_COEX_CHAN_MAX),
 
 #ifdef FEATURE_WLAN_SCAN_PNO
 	REG_VARIABLE(CFG_PNO_CHANNEL_PREDICTION_NAME, WLAN_PARAM_Integer,
@@ -6878,6 +6884,9 @@ void hdd_cfg_print(struct hdd_context *hdd_ctx)
 	hdd_debug("Name = [%s] value = [%u]",
 		  CFG_STA_SAP_SCC_ON_DFS_CHAN,
 		  hdd_ctx->config->sta_sap_scc_on_dfs_chan);
+	hdd_debug("Name = [%s] value = [%u]",
+		  CFG_STA_SAP_SCC_ON_LTE_COEX_CHAN,
+		  hdd_ctx->config->sta_sap_scc_on_lte_coex_chan);
 #ifdef FEATURE_WLAN_SCAN_PNO
 	hdd_debug("Name = [%s] Value = [%u]",
 		   CFG_PNO_CHANNEL_PREDICTION_NAME,
@@ -8426,6 +8435,8 @@ QDF_STATUS hdd_set_policy_mgr_user_cfg(struct hdd_context *hdd_ctx)
 		hdd_ctx->config->sta_sap_scc_on_dfs_chan;
 	user_cfg->channel_select_logic_conc =
 		hdd_ctx->config->channel_select_logic_conc;
+	user_cfg->sta_sap_scc_on_lte_coex_chan =
+		hdd_ctx->config->sta_sap_scc_on_lte_coex_chan;
 	status = policy_mgr_set_user_cfg(hdd_ctx->hdd_psoc, user_cfg);
 	qdf_mem_free(user_cfg);
 
