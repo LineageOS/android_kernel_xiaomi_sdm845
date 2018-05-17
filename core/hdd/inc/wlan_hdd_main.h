d91c352fcf11c3392b06108c17204327f13edfec
Min Liu <minliu@codeaurora.org>
2018-05-17 11:51:53 +0800

    qcacld-3.0: Cleanup blocked scan requests
    The following memory leak issues of blocked scan requests
    need to be addressed:
    
    1. Add list for blocked scan requests
    
    There could be multiple scan requests are blocked before related
    callback can be executed. Currently there is only one pointer
    for such requests. A list is added accordingly.
    
    2. Cleanup blocked scan request when ifdown
    
    Scheduled work for blocked scan might not be able to be executed
    before ifdown. When the work is cancelled, related scan request is
    not freed and will caused memory leak.
    
    Call the relate callback when blocked scan work is cancelled to
    cleanup the pending scan request.
    
    Change-Id: Ifb5fc1b14a043ad67e4ba1d305ce4133b471188c
    CRs-Fixed: 2166111

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -1198,8 +1198,6 @@ struct hdd_adapter {
 
 	/* TODO Move this to sta Ctx */
 	struct wireless_dev wdev;
-	struct cfg80211_scan_request *request;
-	struct cfg80211_scan_request *vendor_request;
 
 	/** ops checks if Opportunistic Power Save is Enable or Not
 	 * ctw stores ctWindow value once we receive Opps command from
@@ -1334,6 +1332,8 @@ struct hdd_adapter {
 	struct delayed_work acs_pending_work;
 
 	struct work_struct scan_block_work;
+	qdf_list_t blocked_scan_request_q;
+	qdf_mutex_t blocked_scan_request_q_lock;
 #ifdef MSM_PLATFORM
 	unsigned long prev_rx_packets;
 	unsigned long prev_tx_packets;
