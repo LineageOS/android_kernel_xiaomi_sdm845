41eedd4c4b49eeca5b40cb9d676bf8fa4fe41e8b
Bruno Martins <bgcngm@gmail.com>
2018-06-17 23:47:34 +0100

    techpack: asoc: Include Elliptic Labs ultrasound proximity support
    Change-Id: I955379223313f1f127e3309be60b41ed3457a7b0

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -39,6 +39,8 @@
 #include <dsp/q6lsm.h>
 #include <dsp/q6core.h>
 #include <dsp/audio_cal_utils.h>
+#include <dsp/apr_elliptic.h>
+#include <elliptic/elliptic_mixer_controls.h>
 
 #include "msm-pcm-routing-v2.h"
 #include "msm-pcm-routing-devdep.h"
@@ -19547,6 +19549,7 @@ static int msm_routing_probe(struct snd_soc_platform *platform)
 					ARRAY_SIZE(aptx_dec_license_controls));
 	snd_soc_add_platform_controls(platform, stereo_channel_reverse_control,
 				ARRAY_SIZE(stereo_channel_reverse_control));
+	elliptic_add_platform_controls(platform);
 	return 0;
 }
 
