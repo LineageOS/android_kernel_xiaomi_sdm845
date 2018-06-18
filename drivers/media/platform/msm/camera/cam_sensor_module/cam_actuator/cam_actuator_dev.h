0f01f1ad64486ef1eb4e2efbb1144cc7c7ffc55e
Bruno Martins <bgcngm@gmail.com>
2018-06-18 16:36:11 +0200

    drivers: media: camera: Import Xiaomi changes over camera stack
    Change-Id: Ie7dbe07556f24c02641ebeaccaffb12181c02d41

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_actuator/cam_actuator_dev.h
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_actuator/cam_actuator_dev.h
@@ -48,6 +48,12 @@
 #define MSM_ACTUATOR_MAX_VREGS (10)
 #define ACTUATOR_MAX_POLL_COUNT 10
 
+#ifdef CONFIG_USE_ROHM_BU64753
+#define EEPROM_MAP_DATA_CNT 60
+#define EEPROM_READ_START_INDEX 7856
+#define EEPROM_READ_END_INDEX 7915
+#define ROHM_ACTUATOR_II2_ADDR 0x76
+#endif
 
 enum cam_actuator_apply_state_t {
 	ACT_APPLY_SETTINGS_NOW,
@@ -125,6 +131,7 @@ struct cam_actuator_ctrl_t {
 	struct i2c_data_settings i2c_data;
 	struct cam_actuator_query_cap act_info;
 	struct intf_params bridge_intf;
+	struct platform_device *pdev;
 };
 
 #endif /* _CAM_ACTUATOR_DEV_H_ */
