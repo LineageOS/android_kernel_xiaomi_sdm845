004921f1191f258ad44149858f812b504545820d
Yaroslav Furman <yaro330@gmail.com>
2018-06-21 23:04:25 +0300

    tcp_westwood: tune up a bit
    initial values were way too agressive.
    Let's et them closer to default values.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Change-Id: Ia933e80f58d318d3224d55fa85b0d02ec9c5ca3d
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: awkoo <awkooox@gmail.com>

--- net/ipv4/tcp_westwood.c
+++ net/ipv4/tcp_westwood.c
@@ -43,8 +43,8 @@ struct westwood {
 };
 
 /* TCP Westwood functions and constants */
-int tcp_westwood_rtt_min = 22;
-int tcp_westwood_init_rtt = 18;
+int tcp_westwood_rtt_min = 30;
+int tcp_westwood_init_rtt = 200;
 
 /* Let's make them tunable */
 module_param_named(rtt_min, tcp_westwood_rtt_min, int, 0644);
