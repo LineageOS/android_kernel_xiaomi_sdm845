77e1ad6001a69a295e4309dd9fd049bded225e7b
Steve Pfetsch <spfetsch@google.com>
2018-06-21 15:35:07 -0700

    input: touchscreen: fts_521: do not temporarily unregister drm notifier
    The stm_fts_cmd handler unregisters and later re-registers the drm
    notifier to prevent a the bus master change while processing a sysfs
    command. This is no longer necessary because the bus will held active
    at least until the sysfs bus reference is released.
    
    Bug: 110815748
    Change-Id: Ia5835a4f13bfeee051013d60956abd4aa8bbc5ef
    Signed-off-by: Steve Pfetsch <spfetsch@google.com>

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -1099,13 +1099,7 @@ static ssize_t stm_fts_cmd_show(struct device *dev,
 			res = (res | ERROR_DISABLE_INTER);
 			goto END;
 		}
-#ifdef CONFIG_DRM
-		res = drm_unregister_client(&info->notifier);
-		if (res < 0) {
-			pr_err("ERROR: unregister notifier failed!\n");
-			goto END;
-		}
-#endif
+
 		switch (typeOfComand[0]) {
 			/*ITO TEST */
 		case 0x01:
@@ -1311,11 +1305,7 @@ static ssize_t stm_fts_cmd_show(struct device *dev,
 		res = ERROR_OP_NOT_ALLOW;
 
 	}
-#ifdef CONFIG_DRM
-	if (drm_register_client(&info->notifier) < 0) {
-		pr_err("ERROR: register notifier failed!\n");
-	}
-#endif
+
 END:
 	size = PAGE_SIZE;
 	index = 0;
