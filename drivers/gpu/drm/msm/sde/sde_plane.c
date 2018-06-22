276d4f0a2ca001b1cf3c02fb4e50d71f4144030a
Bruno Martins <bgcngm@gmail.com>
2018-06-23 00:50:58 +0200

    drivers: gpu: drm: Import Xiaomi changes
    Change-Id: I19c553a5ba8decee48838689f62052ae3fa386c1

--- drivers/gpu/drm/msm/sde/sde_plane.c
+++ drivers/gpu/drm/msm/sde/sde_plane.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014-2018 The Linux Foundation. All rights reserved.
+ * Copyright (C) 2018 XiaoMi, Inc.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
  *
@@ -3257,7 +3258,8 @@ static void _sde_plane_sspp_atomic_check_mode_changed(struct sde_plane *psde,
 
 	if (!fb || !old_fb) {
 		SDE_DEBUG_PLANE(psde, "can't compare fb handles\n");
-	} else if (fb->pixel_format != old_fb->pixel_format) {
+	} else if ((fb->pixel_format != old_fb->pixel_format) ||
+			pstate->const_alpha_en != old_pstate->const_alpha_en) {
 		SDE_DEBUG_PLANE(psde, "format change\n");
 		pstate->dirty |= SDE_PLANE_DIRTY_FORMAT | SDE_PLANE_DIRTY_RECTS;
 	} else {
