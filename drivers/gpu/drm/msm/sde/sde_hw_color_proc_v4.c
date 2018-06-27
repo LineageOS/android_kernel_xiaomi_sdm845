10e1f813221d1905b2d71049581f90cf0da24970
Francisco Franco <franciscofranco.1990@gmail.com>
2018-06-27 00:53:49 +0000

    drm: msm: kcal: Make the userspace module param vars static and use short instead of integers
    Same reasoning as https://github.com/franciscofranco/enchilada/commit/20461e4aef0b54ea11c84223db4f59de14aabe57
    
    There's no point in using integers where a short is big enough.
    
    Change-Id: If6575e3d7719c0754920c0eb74b86b3e0163f1be
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
+++ drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
@@ -15,21 +15,21 @@
 #include "sde_hw_color_proc_common_v4.h"
 #include "sde_hw_color_proc_v4.h"
 
-unsigned int kcal_red = 256;
-unsigned int kcal_green = 256;
-unsigned int kcal_blue = 256;
-unsigned int kcal_hue = 0;
-unsigned int kcal_sat = 255;
-unsigned int kcal_val = 255;
-unsigned int kcal_cont = 255;
+static unsigned short kcal_red = 256;
+static unsigned short kcal_green = 256;
+static unsigned short kcal_blue = 256;
+static unsigned short kcal_hue = 0;
+static unsigned short kcal_sat = 255;
+static unsigned short kcal_val = 255;
+static unsigned short kcal_cont = 255;
 
-module_param(kcal_red, uint, 0644);
-module_param(kcal_green, uint, 0644);
-module_param(kcal_blue, uint, 0644);
-module_param(kcal_hue, uint, 0644);
-module_param(kcal_sat, uint, 0644);
-module_param(kcal_val, uint, 0644);
-module_param(kcal_cont, uint, 0644);
+module_param(kcal_red, short, 0644);
+module_param(kcal_green, short, 0644);
+module_param(kcal_blue, short, 0644);
+module_param(kcal_hue, short, 0644);
+module_param(kcal_sat, short, 0644);
+module_param(kcal_val, short, 0644);
+module_param(kcal_cont, short, 0644);
 
 static int sde_write_3d_gamut(struct sde_hw_blk_reg_map *hw,
 		struct drm_msm_3d_gamut *payload, u32 base,
