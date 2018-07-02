c14a76727e8754f3dde43f17b8b339b57dda3b82
Min Liu <minliu@codeaurora.org>
2018-07-02 18:01:02 +0800

    qcacld-3.0: Reset IEs when SAP failed to be started
    In wlan_hdd_cfg80211_start_bss, not all the error path will
    reset IEs, which might lead to memory leak of IE buffers.
    
    Update error handling of the function to ensure that it will
    cover all error cases.
    
    Change-Id: I44d1eeb4d0100a2f720ea20abd5eacf9fa9bf1fc
    CRs-Fixed: 2271222

--- core/hdd/src/wlan_hdd_hostapd.c
+++ core/hdd/src/wlan_hdd_hostapd.c
@@ -7906,7 +7906,6 @@ int wlan_hdd_cfg80211_start_bss(struct hdd_adapter *adapter,
 
 	if (0 != wlan_hdd_cfg80211_update_apies(adapter)) {
 		hdd_err("SAP Not able to set AP IEs");
-		wlansap_reset_sap_config_add_ie(pConfig, eUPDATE_IE_ALL);
 		ret = -EINVAL;
 		goto error;
 	}
@@ -8001,7 +8000,6 @@ int wlan_hdd_cfg80211_start_bss(struct hdd_adapter *adapter,
 	if (!QDF_IS_STATUS_SUCCESS(status)) {
 		mutex_unlock(&hdd_ctx->sap_lock);
 
-		wlansap_reset_sap_config_add_ie(pConfig, eUPDATE_IE_ALL);
 		hdd_set_connection_in_progress(false);
 		hdd_err("SAP Start Bss fail");
 		ret = -EINVAL;
@@ -8064,6 +8062,7 @@ error:
 	clear_bit(SOFTAP_INIT_DONE, &adapter->event_flags);
 	qdf_atomic_set(&adapter->session.ap.acs_in_progress, 0);
 	wlan_hdd_undo_acs(adapter);
+	wlansap_reset_sap_config_add_ie(pConfig, eUPDATE_IE_ALL);
 
 free:
 	/* Enable Roaming after start bss in case of failure/success */
