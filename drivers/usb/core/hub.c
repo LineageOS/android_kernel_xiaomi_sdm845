ec0f5e45813f1602892b18a3cb0d78e7aa6d7a67
Bruno Martins <bgcngm@gmail.com>
2018-07-03 14:37:49 +0200

    drivers: usb: Import misc Xiaomi changes over USB stack
    Change-Id: I590db50912264da32a521e8b3c0442d089053b4e

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4344,6 +4344,7 @@ static int hub_set_address(struct usb_device *udev, int devnum)
 	return retval;
 }
 
+#ifndef CONFIG_MACH_XIAOMI
 /*
  * There are reports of USB 3.0 devices that say they support USB 2.0 Link PM
  * when they're plugged into a USB 2.0 port, but they don't work when LPM is
@@ -4370,6 +4371,7 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 		usb_enable_usb2_hardware_lpm(udev);
 	}
 }
+#endif
 
 static int hub_enable_device(struct usb_device *udev)
 {
@@ -4703,7 +4705,10 @@ hub_port_init(struct usb_hub *hub, struct usb_device *udev, int port1,
 	/* notify HCD that we have a device connected and addressed */
 	if (hcd->driver->update_device)
 		hcd->driver->update_device(hcd, udev);
+	/* disable USB 2.0 Link PM to fix long time usb storage recognition issue */
+#ifndef CONFIG_MACH_XIAOMI
 	hub_set_initial_usb2_lpm_policy(udev);
+#endif
 fail:
 	if (retval) {
 		hub_port_disable(hub, port1, 0);
