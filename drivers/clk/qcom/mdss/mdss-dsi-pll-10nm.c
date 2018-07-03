4b7519e10713d554c24b340a6e52c3d71ddd29c4
Bruno Martins <bgcngm@gmail.com>
2018-07-03 15:23:35 +0200

    drivers: clk: qcom: Import Xiaomi changes for 10nm DSI PLL
    Change-Id: I4148a9d520fd47481808094a10544a4bfd7446bb

--- drivers/clk/qcom/mdss/mdss-dsi-pll-10nm.c
+++ drivers/clk/qcom/mdss/mdss-dsi-pll-10nm.c
@@ -959,7 +959,7 @@ static int dsi_pll_10nm_lock_status(struct mdss_pll_resources *pll)
 				       ((status & BIT(0)) > 0),
 				       delay_us,
 				       timeout_us);
-	if (rc)
+	if (rc && !pll->handoff_resources)
 		pr_err("DSI PLL(%d) lock failed, status=0x%08x\n",
 			pll->index, status);
 
@@ -1213,7 +1213,7 @@ static unsigned long vco_10nm_recalc_rate(struct clk_hw *hw,
 	struct mdss_pll_resources *pll = vco->priv;
 	int rc;
 	u64 ref_clk = vco->ref_clk_rate;
-	u64 vco_rate;
+	u64 vco_rate = 0;
 	u64 multiplier;
 	u32 frac;
 	u32 dec;
@@ -1248,8 +1248,13 @@ static unsigned long vco_10nm_recalc_rate(struct clk_hw *hw,
 		return 0;
 	}
 
-	if (!dsi_pll_10nm_lock_status(pll))
-		pll->handoff_resources = true;
+	pll->handoff_resources = true;
+	if (dsi_pll_10nm_lock_status(pll)) {
+		pr_debug("PLL not enabled\n");
+		pll->handoff_resources = false;
+		goto end;
+	}
+
 
 	dec = MDSS_PLL_REG_R(pll->pll_base, PLL_DECIMAL_DIV_START_1);
 	dec &= 0xFF;
@@ -1284,6 +1289,7 @@ static unsigned long vco_10nm_recalc_rate(struct clk_hw *hw,
 
 	(void)mdss_pll_resource_enable(pll, false);
 
+end:
 	return (unsigned long)vco_rate;
 }
 
