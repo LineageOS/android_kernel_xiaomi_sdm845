db587fdcf12cab6b1edd279c2a7c0d2b98ed4294
Jeff Johnson <jjohnson@codeaurora.org>
2018-07-23 21:07:53 -0700

    qcacld-3.0: Remove ftm_msg_processor_callback from MAC context
    The MAC context ftm_msg_processor_callback is a remnant from the
    legacy FTM support. Since this callback is no longer used, remove it.
    
    Change-Id: Ibd375bcd91900654315681f7f9b56bf100090942
    CRs-Fixed: 2298895

--- core/mac/inc/ani_global.h
+++ core/mac/inc/ani_global.h
@@ -880,7 +880,6 @@ typedef struct sAniSirGlobal {
 	csr_readyToExtWoWCallback readyToExtWoWCallback;
 	void *readyToExtWoWContext;
 #endif
-	hdd_ftm_msg_processor ftm_msg_processor_callback;
 	struct vdev_type_nss vdev_type_nss_2g;
 	struct vdev_type_nss vdev_type_nss_5g;
 
