77cc156bb15b72c31d973468c55169c632cd0478
nshrivas <nshrivas@codeaurora.org>
2018-09-05 10:26:05 -0700

    Release 5.2.02.7V
    Release 5.2.02.7V
    
    Change-Id: I57222c61cda3b718c78adab8b23dff0022b24c5e
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            02
-#define QWLAN_VERSION_EXTRA            "U"
+#define QWLAN_VERSION_EXTRA            "V"
 #define QWLAN_VERSION_BUILD            7
 
-#define QWLAN_VERSIONSTR               "5.2.02.7U"
+#define QWLAN_VERSIONSTR               "5.2.02.7V"
 
 #endif /* QWLAN_VERSION_H */
