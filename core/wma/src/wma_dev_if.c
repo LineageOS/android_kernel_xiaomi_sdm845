c3062391b6884dfce40480ea4d3db18a4da8ad44
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2018-09-26 15:52:56 +0530

    qcacld-3.0: Fix error handling in eWNI_SME_ADD_STA_SELF_RSP
    When peer creation fails in wma_create_peer, vdev delete is sent
    to the fw and then eWNI_SME_ADD_STA_SELF_RSP is sent to sme.
    Here three error cases needs to be handled:
    1. The vdev deletion has happened, but the cdp_detach_peer is
    not done. So the data path peer remains. Also the vdev_active
    flag that was set after vdev creation was not unset.
    2. The eWNI_SME_ADD_STA_SELF_RSP msg handler
    csr_process_add_sta_session_rsp invokes csr_roam_session_opened
    which signals HDD that vdev is created successfully and hdd
    calls hdd_vdev_ready and vdev related parameter set commands are
    sent to FW for the deleted vdev.
    3. Vdev delete is not sent for objmgr peer creation failure in
    case of VDEV_TYPE_STA and release vdev object ref count.
    
    Add cdp_vdev_detach() calls during error case and set the
    vdev_active flag to false. Handle releasing vdev object ref
    count in all needed error case flows.
    Propagate the error in wma_vdev_attach() through
    csr_roam_session_opened() to HDD and abort the vdev create.
    
    Change-Id: Iec97122d011098fae7ae2a59864fbe8ca8a0980e
    CRs-Fixed: 2322212

--- core/wma/src/wma_dev_if.c
+++ core/wma/src/wma_dev_if.c
@@ -2211,12 +2211,13 @@ struct cdp_vdev *wma_vdev_attach(tp_wma_handle wma_handle,
 	params.nss_5g = self_sta_req->nss_5g;
 
 	/* Create a vdev in target */
-	status = wmi_unified_vdev_create_send(wma_handle->wmi_handle,
-						self_sta_req->self_mac_addr,
-						&params);
-	if (QDF_IS_STATUS_ERROR(status)) {
+	ret = wmi_unified_vdev_create_send(wma_handle->wmi_handle,
+					   self_sta_req->self_mac_addr,
+					   &params);
+	if (QDF_IS_STATUS_ERROR(ret)) {
 		WMA_LOGE("%s: Unable to add an interface for ath_dev",
 			 __func__);
+		status = QDF_STATUS_E_FAILURE;
 		goto end;
 	}
 
@@ -2233,7 +2234,7 @@ struct cdp_vdev *wma_vdev_attach(tp_wma_handle wma_handle,
 					   self_sta_req->self_mac_addr,
 					   vdev_id, txrx_vdev_type);
 
-	WMA_LOGD("vdev_id %hu, txrx_vdev_handle = %pK", self_sta_req->session_id,
+	WMA_LOGD("vdev_id %hu, txrx_vdev_handle = %pK", vdev_id,
 		 txrx_vdev_handle);
 
 	if (NULL == txrx_vdev_handle) {
@@ -2280,9 +2281,9 @@ struct cdp_vdev *wma_vdev_attach(tp_wma_handle wma_handle,
 	tx_rx_aggregation_size.vdev_id = self_sta_req->session_id;
 	tx_rx_aggregation_size.aggr_type = WMI_VDEV_CUSTOM_AGGR_TYPE_AMPDU;
 
-	status = wma_set_tx_rx_aggregation_size(&tx_rx_aggregation_size);
-	if (status != QDF_STATUS_SUCCESS)
-		WMA_LOGE("failed to set aggregation sizes(err=%d)", status);
+	ret = wma_set_tx_rx_aggregation_size(&tx_rx_aggregation_size);
+	if (QDF_IS_STATUS_ERROR(ret))
+		WMA_LOGE("failed to set aggregation sizes(err=%d)", ret);
 
 	tx_rx_aggregation_size.tx_aggregation_size_be =
 				self_sta_req->tx_aggregation_size_be;
@@ -2306,11 +2307,10 @@ struct cdp_vdev *wma_vdev_attach(tp_wma_handle wma_handle,
 
 	switch (self_sta_req->type) {
 	case WMI_VDEV_TYPE_STA:
-		status = wma_set_tx_rx_aggregation_size_per_ac(
+		ret = wma_set_tx_rx_aggregation_size_per_ac(
 						&tx_rx_aggregation_size);
-		if (status != QDF_STATUS_SUCCESS)
-			WMA_LOGE("failed to set aggr sizes per ac(err=%d)",
-				 status);
+		if (QDF_IS_STATUS_ERROR(ret))
+			WMA_LOGE("set aggr sizes per ac(err=%d) failed", ret);
 
 		if (wlan_cfg_get_int(mac, WNI_CFG_INFRA_STA_KEEP_ALIVE_PERIOD,
 				     &cfg_val) != QDF_STATUS_SUCCESS) {
@@ -2326,57 +2326,55 @@ struct cdp_vdev *wma_vdev_attach(tp_wma_handle wma_handle,
 		/* offload STA SA query related params to fwr */
 		if (wmi_service_enabled(wma_handle->wmi_handle,
 			wmi_service_sta_pmf_offload)) {
-			wma_set_sta_sa_query_param(wma_handle,
-						   self_sta_req->session_id);
+			wma_set_sta_sa_query_param(wma_handle, vdev_id);
 		}
 
-		status = wma_set_sw_retry_threshold(
-						&tx_aggr_sw_retry_threshold);
-		if (status != QDF_STATUS_SUCCESS)
-			WMA_LOGE("failed to set retry threshold(err=%d)",
-				 status);
+		ret = wma_set_sw_retry_threshold(&tx_aggr_sw_retry_threshold);
+		if (QDF_IS_STATUS_ERROR(ret))
+			WMA_LOGE("failed to set retry threshold(err=%d)", ret);
 		break;
 	}
 
-	wma_handle->interfaces[self_sta_req->session_id].type =
-		self_sta_req->type;
-	wma_handle->interfaces[self_sta_req->session_id].sub_type =
-		self_sta_req->sub_type;
-	qdf_atomic_init(&wma_handle->interfaces
-			[self_sta_req->session_id].bss_status);
+	wma_handle->interfaces[vdev_id].type = self_sta_req->type;
+	wma_handle->interfaces[vdev_id].sub_type = self_sta_req->sub_type;
+	qdf_atomic_init(&wma_handle->interfaces[vdev_id].bss_status);
 
 	if (wma_vdev_uses_self_peer(self_sta_req->type,
 				    self_sta_req->sub_type)) {
-		status = wma_create_peer(wma_handle, txrx_pdev,
-					 txrx_vdev_handle,
-					 self_sta_req->self_mac_addr,
-					 WMI_PEER_TYPE_DEFAULT,
-					 self_sta_req->session_id, false);
-		if (status != QDF_STATUS_SUCCESS) {
+		ret = wma_create_peer(wma_handle, txrx_pdev, txrx_vdev_handle,
+				      self_sta_req->self_mac_addr,
+				      WMI_PEER_TYPE_DEFAULT,
+				      self_sta_req->session_id, false);
+		if (QDF_IS_STATUS_ERROR(ret)) {
 			WMA_LOGE("%s: Failed to create peer", __func__);
 			status = QDF_STATUS_E_FAILURE;
 			wmi_unified_vdev_delete_send(wma_handle->wmi_handle,
 						     self_sta_req->session_id);
+			wma_handle->interfaces[vdev_id].vdev_active = false;
+			wma_cdp_vdev_detach(soc, wma_handle, vdev_id);
 			goto end;
 		}
 	} else if (self_sta_req->type == WMI_VDEV_TYPE_STA) {
-		obj_peer = wma_create_objmgr_peer(wma_handle,
-						  vdev_id,
+		obj_peer = wma_create_objmgr_peer(wma_handle, vdev_id,
 						  self_sta_req->self_mac_addr,
 						  WMI_PEER_TYPE_DEFAULT);
 		if (!obj_peer) {
 			WMA_LOGE("%s: Failed to create obj mgr peer for self sta",
 				 __func__);
+			status = QDF_STATUS_E_FAILURE;
+			wmi_unified_vdev_delete_send(wma_handle->wmi_handle,
+						     self_sta_req->session_id);
+			wma_handle->interfaces[vdev_id].vdev_active = false;
+			wma_cdp_vdev_detach(soc, wma_handle, vdev_id);
 			goto end;
 		}
 	}
 
 	WMA_LOGD("Setting WMI_VDEV_PARAM_DISCONNECT_TH: %d",
-		self_sta_req->pkt_err_disconn_th);
-	ret = wma_vdev_set_param(wma_handle->wmi_handle,
-				self_sta_req->session_id,
-				WMI_VDEV_PARAM_DISCONNECT_TH,
-				self_sta_req->pkt_err_disconn_th);
+		 self_sta_req->pkt_err_disconn_th);
+	ret = wma_vdev_set_param(wma_handle->wmi_handle, vdev_id,
+				 WMI_VDEV_PARAM_DISCONNECT_TH,
+				 self_sta_req->pkt_err_disconn_th);
 	if (ret)
 		WMA_LOGE("Failed to set WMI_VDEV_PARAM_DISCONNECT_TH");
 
@@ -2388,8 +2386,7 @@ struct cdp_vdev *wma_vdev_attach(tp_wma_handle wma_handle,
 	if (QDF_IS_STATUS_ERROR(ret))
 		WMA_LOGE("Failed to set WMI VDEV MCC_RTSCTS_PROTECTION_ENABLE");
 
-	ret = wma_vdev_set_param(wma_handle->wmi_handle,
-			self_sta_req->session_id,
+	ret = wma_vdev_set_param(wma_handle->wmi_handle, vdev_id,
 			WMI_VDEV_PARAM_MCC_BROADCAST_PROBE_ENABLE,
 			mac->roam.configParam.mcc_bcast_prob_resp_enable);
 	if (QDF_IS_STATUS_ERROR(ret))
@@ -2509,7 +2506,7 @@ struct cdp_vdev *wma_vdev_attach(tp_wma_handle wma_handle,
 		WMA_LOGD("%s: setting ini value for WNI_CFG_ENABLE_MCC_ADAPTIVE_SCHED: %d",
 			__func__, cfg_val);
 		ret = wma_set_enable_disable_mcc_adaptive_scheduler(cfg_val);
-		if (ret != QDF_STATUS_SUCCESS) {
+		if (QDF_IS_STATUS_ERROR(ret)) {
 			WMA_LOGE("Failed to set WNI_CFG_ENABLE_MCC_ADAPTIVE_SCHED");
 		}
 	} else {
@@ -2517,9 +2514,9 @@ struct cdp_vdev *wma_vdev_attach(tp_wma_handle wma_handle,
 	}
 
 	if (self_sta_req->type == WMI_VDEV_TYPE_STA) {
-		status = wma_config_active_apf_mode(wma_handle,
-						    self_sta_req->session_id);
-		if (QDF_IS_STATUS_ERROR(status))
+		ret = wma_config_active_apf_mode(wma_handle,
+						 self_sta_req->session_id);
+		if (QDF_IS_STATUS_ERROR(ret))
 			WMA_LOGE("Failed to configure active APF mode");
 	}
 
