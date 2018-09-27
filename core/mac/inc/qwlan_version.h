82f7be154c54b555739677fafc1b07d64cc7aa40
nshrivas <nshrivas@codeaurora.org>
2018-09-27 15:46:38 -0700

    Release 5.2.02.10H
    Release 5.2.02.10H
    
    Change-Id: Ie856cdd2460168340f6ff5c6417bb0b41a3b5018
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            02
-#define QWLAN_VERSION_EXTRA            "G"
+#define QWLAN_VERSION_EXTRA            "H"
 #define QWLAN_VERSION_BUILD            10
 
-#define QWLAN_VERSIONSTR               "5.2.02.10G"
+#define QWLAN_VERSIONSTR               "5.2.02.10H"
 
 #endif /* QWLAN_VERSION_H */
