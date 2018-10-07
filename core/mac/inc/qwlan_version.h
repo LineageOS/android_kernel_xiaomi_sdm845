11254b6faae82bf926b5f518fbf365300685525f
nshrivas <nshrivas@codeaurora.org>
2018-10-07 11:47:59 -0700

    Release 5.2.02.11J
    Release 5.2.02.11J
    
    Change-Id: I5e6360245cd4063809fec436ad264ba3e1428ceb
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            02
-#define QWLAN_VERSION_EXTRA            "I"
+#define QWLAN_VERSION_EXTRA            "J"
 #define QWLAN_VERSION_BUILD            11
 
-#define QWLAN_VERSIONSTR               "5.2.02.11I"
+#define QWLAN_VERSIONSTR               "5.2.02.11J"
 
 #endif /* QWLAN_VERSION_H */
