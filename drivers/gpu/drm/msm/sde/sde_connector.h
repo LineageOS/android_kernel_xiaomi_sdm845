78679d131dbaaa7cb009d439d6d560519a3ff7b8
Ajay Singh Parmar <aparmar@codeaurora.org>
2018-10-22 12:24:25 -0700

    drm/msm/sde: update hdr properties
    Update the HDR metadata properties even though HDR is not
    supported. This is needed as subsequent hot plugs may or
    may not have HDR supported. Update the HDR metadata to
    avoid using residue data from previous session.
    
    CRs-Fixed: 2339345
    Change-Id: Iaba37b7ea6208dcbbe5d635decd536eae1d7a096
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_connector.h
+++ drivers/gpu/drm/msm/sde/sde_connector.h
@@ -357,6 +357,7 @@ struct sde_connector_evt {
  * @unset_bl_level: BL level that needs to be set later
  * @allow_bl_update: Flag to indicate if BL update is allowed currently or not
  * @last_cmd_tx_sts: status of the last command transfer
+ * @hdr_capable: external hdr support present
  */
 struct sde_connector {
 	struct drm_connector base;
@@ -404,6 +405,7 @@ struct sde_connector {
 	bool allow_bl_update;
 
 	bool last_cmd_tx_sts;
+	bool hdr_capable;
 	bool panel_dead_skip;
 };
 
