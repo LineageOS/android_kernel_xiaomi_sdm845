2aa4331195cf8a854f9aa17ee8479827f4a640f6
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2018-10-23 16:49:19 +0530

    qcacmn: SSID with zero or space should be NULL ssid
    Last frame received in scan before the connect is
    beacon with NULL ssid with non zero length and ssid
    is sent as NULL to supplicant after association. This
    will result in supplicant to trigger disconnect
    casuing delay in reconnection.
    
    Fix is to consider ssid with zeros or spaces as null
    ssid.
    
    Change-Id: If96776ae85926948d714e975c3e9b4011e8a20b3
    CRs-Fixed: 2330485

--- umac/scan/dispatcher/inc/wlan_scan_utils_api.h
+++ umac/scan/dispatcher/inc/wlan_scan_utils_api.h
@@ -31,6 +31,8 @@
 #include <wlan_scan_public_structs.h>
 #include<wlan_mgmt_txrx_utils_api.h>
 
+#define ASCII_SPACE_CHARACTER 32
+
 /**
  * util_is_scan_entry_match() - func to check if both scan entry
  * are from same AP
@@ -268,10 +270,6 @@ static inline bool
 util_is_ssid_match(struct wlan_ssid *ssid1,
 		struct wlan_ssid *ssid2)
 {
-
-	if (ssid1->length == 0)
-		return true;
-
 	if (ssid1->length != ssid2->length)
 		return false;
 
@@ -1478,5 +1476,40 @@ enum wlan_band util_scan_scm_freq_to_band(uint16_t freq);
  * Return: true if scan complete, false otherwise
  */
 bool util_is_scan_completed(struct scan_event *event, bool *success);
+
+/**
+ * util_scan_is_null_ssid() - to check for NULL ssid
+ * @ssid: ssid
+ *
+ * Return: true if NULL ssid else false
+ */
+static inline bool util_scan_is_null_ssid(struct wlan_ssid *ssid)
+{
+	uint32_t ssid_length;
+	uint8_t *ssid_str;
+
+	if (ssid->length == 0)
+		return true;
+
+	/* Consider 0 or space for hidden SSID */
+	if (0 == ssid->ssid[0])
+		return true;
+
+	ssid_length = ssid->length;
+	ssid_str = ssid->ssid;
+
+	while (ssid_length) {
+		if (*ssid_str != ASCII_SPACE_CHARACTER &&
+		    *ssid_str)
+			break;
+		ssid_str++;
+		ssid_length--;
+	}
+
+	if (ssid_length == 0)
+		return true;
+
+	return false;
+}
 
 #endif
