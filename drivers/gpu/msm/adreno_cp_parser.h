a213df1da25afa256424010849da344c47dde162
kdrag0n <dragon@khronodragon.com>
2018-11-12 21:43:38 -0800

    gpu: adreno: only compile Adrneo 6xx driver
    We won't be using this with an Adreno 3xx/4xx/5xx GPU.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>

--- drivers/gpu/msm/adreno_cp_parser.h
+++ drivers/gpu/msm/adreno_cp_parser.h
@@ -15,9 +15,6 @@
 
 #include "adreno.h"
 
-extern const unsigned int a3xx_cp_addr_regs[];
-extern const unsigned int a4xx_cp_addr_regs[];
-
 /*
  * struct adreno_ib_object - Structure containing information about an
  * address range found in an IB
@@ -134,15 +131,7 @@ static inline void adreno_ib_init_ib_obj(uint64_t gpuaddr,
 static inline int adreno_cp_parser_getreg(struct adreno_device *adreno_dev,
 					enum adreno_cp_addr_regs reg_enum)
 {
-	if (reg_enum == ADRENO_CP_ADDR_MAX)
-		return -EEXIST;
-
-	if (adreno_is_a3xx(adreno_dev))
-		return a3xx_cp_addr_regs[reg_enum];
-	else if (adreno_is_a4xx(adreno_dev))
-		return a4xx_cp_addr_regs[reg_enum];
-	else
-		return -EEXIST;
+	return -EEXIST;
 }
 
 /*
@@ -160,19 +149,6 @@ static inline int adreno_cp_parser_regindex(struct adreno_device *adreno_dev,
 				enum adreno_cp_addr_regs start,
 				enum adreno_cp_addr_regs end)
 {
-	int i;
-	const unsigned int *regs;
-
-	if (adreno_is_a4xx(adreno_dev))
-		regs = a4xx_cp_addr_regs;
-	else if (adreno_is_a3xx(adreno_dev))
-		regs = a3xx_cp_addr_regs;
-	else
-		return -EEXIST;
-
-	for (i = start; i <= end && i < ADRENO_CP_ADDR_MAX; i++)
-		if (regs[i] == offset)
-			return i;
 	return -EEXIST;
 }
 
