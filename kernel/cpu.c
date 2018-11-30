12ebdcd682a63514c58efd054f676b867f0044a5
Sultan Alsawaf <sultan@kerneltoast.com>
2018-11-30 02:37:09 -0800

    cpumask: Add cpumasks for big and little CPU clusters
    Add cpu_lp_mask and cpu_perf_mask to represent the CPUs that belong to
    each cluster in a dual-cluster, heterogeneous system.
    
    Note that this assumes that CONFIG_NR_CPUS is set to exatly the number of
    CPUs present, and that the number of CPUs per cluster is split evenly in
    half.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Yousef <yusufgadrie@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2274,6 +2274,21 @@ EXPORT_SYMBOL(__cpu_active_mask);
 struct cpumask __cpu_isolated_mask __read_mostly;
 EXPORT_SYMBOL(__cpu_isolated_mask);
 
+/*
+ * This assumes that half of the CPUs are little and that they have lower
+ * CPU numbers than the big CPUs (e.g., on an 8-core system, CPUs 0-3 would be
+ * little and CPUs 4-7 would be big).
+ */
+#define LITTLE_CPU_MASK ((1UL << (NR_CPUS / 2)) - 1)
+#define BIG_CPU_MASK    (((1UL << NR_CPUS) - 1) & ~LITTLE_CPU_MASK)
+static const unsigned long little_cluster_cpus = LITTLE_CPU_MASK;
+const struct cpumask *const cpu_lp_mask = to_cpumask(&little_cluster_cpus);
+EXPORT_SYMBOL(cpu_lp_mask);
+
+static const unsigned long big_cluster_cpus = BIG_CPU_MASK;
+const struct cpumask *const cpu_perf_mask = to_cpumask(&big_cluster_cpus);
+EXPORT_SYMBOL(cpu_perf_mask);
+
 void init_cpu_present(const struct cpumask *src)
 {
 	cpumask_copy(&__cpu_present_mask, src);
