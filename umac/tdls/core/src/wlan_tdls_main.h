ce5c847e086a4ef7e68b9930a67af46718e35a3a
gaurank kathpalia <gkathpal@codeaurora.org>
2018-12-04 19:55:15 +0530

    qcacmn: Fix memory allocation latency in beacon process
    Currently if the number of APs in the STA environment
    are many, then the STA will receive many beacons, whose
    beacon process path can take long time, in the kernel
    work queue, hence the other processes have to wait
    for them to complete, and may get timeout, if the
    the time to process the beaocns is larger than their
    process timeout.
    
    Fix is to :-
    1. Add rate limit to failure conditions of memory
    not allocated
    2. Make memory allocation in path of beacon process
    atomic.
    
    Change-Id: I0b15fd4924bb7d696a33adeb1875f1f9c277db18
    CRs-Fixed: 2360624

--- umac/tdls/core/src/wlan_tdls_main.h
+++ umac/tdls/core/src/wlan_tdls_main.h
@@ -57,7 +57,6 @@
 #define TDLS_DISCOVERY_TIMEOUT_BEFORE_UPDATE     1000
 #define TDLS_SCAN_REJECT_MAX            5
 
-
 #define tdls_log(level, args...) \
 	QDF_TRACE(QDF_MODULE_ID_TDLS, level, ## args)
 #define tdls_logfl(level, format, args...) \
@@ -73,6 +72,8 @@
 	tdls_logfl(QDF_TRACE_LEVEL_ERROR, format, ## args)
 #define tdls_alert(format, args...) \
 	tdls_logfl(QDF_TRACE_LEVEL_FATAL, format, ## args)
+#define tdls_debug_rl(params...) \
+	QDF_TRACE_DEBUG_RL(QDF_MODULE_ID_TDLS, params)
 
 #define TDLS_IS_LINK_CONNECTED(peer)  \
 	((TDLS_LINK_CONNECTED == (peer)->link_status) || \
