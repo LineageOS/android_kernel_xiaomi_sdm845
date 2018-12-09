9766cf0c3a88a9c6ab2ba261409a7334b0a32bd0
kdrag0n <dragon@khronodragon.com>
2018-12-09 12:34:35 -0800

    selinux: avc: Omit audit callbacks if disabled
    Change-Id: I243076c813917379dd01f7cce499ed7f83765afd
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -706,6 +706,7 @@ out:
 	return node;
 }
 
+#ifdef CONFIG_AUDIT
 /**
  * avc_audit_pre_callback - SELinux specific information
  * will be called by generic audit code
@@ -741,7 +742,6 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 	}
 }
 
-#ifdef CONFIG_AUDIT
 /* This is the slow part of avc audit with big stack footprint */
 noinline int slow_avc_audit(u32 ssid, u32 tsid, u16 tclass,
 		u32 requested, u32 audited, u32 denied, int result,
