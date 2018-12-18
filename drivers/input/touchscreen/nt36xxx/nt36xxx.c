6f16619ae5687020f5bfd1ced2dbcb4f91ec82d0
TheScarastic <warabhishek@gmail.com>
2018-12-18 14:23:19 -0500

    touchscreen: nt36xxx: Null terminate attribute group
     * Fixes boot with Clang
    
    Change-Id: I836e9a4b02fdd0191928f1848ee37e530f63045e

--- drivers/input/touchscreen/nt36xxx/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -1591,6 +1591,7 @@ static struct attribute *nvt_attr_group[] = {
 	&dev_attr_panel_vendor.attr,
 	&dev_attr_panel_color.attr,
 	&dev_attr_panel_display.attr,
+	NULL,
 };
 
 /*******************************************************
