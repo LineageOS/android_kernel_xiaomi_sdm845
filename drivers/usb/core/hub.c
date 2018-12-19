4037cb0297c191ff37a42e43e949557f2cbbfe20
Akhil Narang <akhilnarang.1999@gmail.com>
2018-12-19 17:09:15 +0530

    usb: core: fix -Wunused-function warning
    ../drivers/usb/core/hub.c:4349:13: warning: unused function 'hub_set_initial_usb2_lpm_policy' [-Wunused-function]
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: khusika <khusikadhamar@gmail.com>
    Change-Id: I060aa3078eb1d16762e18dc06244edcb01633398

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4358,7 +4358,7 @@ static int hub_set_address(struct usb_device *udev, int devnum)
  * device says it supports the new USB 2.0 Link PM errata by setting the BESL
  * support bit in the BOS descriptor.
  */
-static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
+static void __maybe_unused hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 {
 	struct usb_hub *hub = usb_hub_to_struct_hub(udev->parent);
 	int connect_type = USB_PORT_CONNECT_TYPE_UNKNOWN;
