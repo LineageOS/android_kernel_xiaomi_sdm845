4ed536218704dc3fc7688ba8d488c3a688f4c0ff
Paul Zhang <paulz@codeaurora.org>
2018-12-20 15:12:54 +0800

    qcacld-3.0: Refactor operation to hostscan_adaptive_dwell_mode_no_conn
    Ownership of hostscan_adaptive_dwell_mode_no_conn INI is changed
    from HDD to scan module. So scan module doesn't have to rely on
    HDD to obtain the INI setting.
    
    CRs-Fixed: 2370450
    Change-Id: If3c3d93f0e1763e778eb6e3e70e23231fb01a67e

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -14267,6 +14267,8 @@ static int hdd_update_scan_config(struct hdd_context *hdd_ctx)
 	scan_cfg.scan_bucket_threshold = cfg->first_scan_bucket_threshold;
 	scan_cfg.rssi_cat_gap = cfg->nRssiCatGap;
 	scan_cfg.scan_dwell_time_mode = cfg->scan_adaptive_dwell_mode;
+	scan_cfg.scan_dwell_time_mode_nc =
+		cfg->scan_adaptive_dwell_mode_nc;
 	scan_cfg.is_snr_monitoring_enabled = cfg->fEnableSNRMonitoring;
 	scan_cfg.usr_cfg_probe_rpt_time = cfg->scan_probe_repeat_time;
 	scan_cfg.usr_cfg_num_probes = cfg->scan_num_probes;
