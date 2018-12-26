5e78a1d9cd203dc1bc45a2a277046d3a132d54c1
Yuvraj Saxena <yuvrajx2@gmail.com>
2018-12-26 06:10:14 +0530

    drivers: input: touchscreen: Update to latest Xiaomi sdm845 touchscreen drivers
    * From MiCode/dipper-p-oss release
    
    Change-Id: Ia12a8bded26b56f784af7e223d24851c00bfc79b

--- drivers/input/touchscreen/nt36xxx/nt36xxx_ext_proc.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx_ext_proc.c
@@ -57,17 +57,17 @@ static struct proc_dir_entry *NVT_proc_xiaomi_config_info_entry;
 static struct proc_dir_entry *NVT_proc_xiaomi_lockdown_info_entry;
 
 /* Xiaomi Config Info. */
-static uint8_t nvt_xiaomi_conf_info_fw_ver;
-static uint8_t nvt_xiaomi_conf_info_fae_id;
-static uint64_t nvt_xiaomi_conf_info_reservation;
+static uint8_t nvt_xiaomi_conf_info_fw_ver = 0;
+static uint8_t nvt_xiaomi_conf_info_fae_id = 0;
+static uint64_t nvt_xiaomi_conf_info_reservation = 0;
 /* Xiaomi Lockdown Info */
-static uint8_t tp_maker_cg_lamination;
-static uint8_t display_maker;
-static uint8_t cg_ink_color;
-static uint8_t hw_version;
-static uint16_t project_id;
-static uint8_t cg_maker;
-static uint8_t reservation_byte;
+static uint8_t tp_maker_cg_lamination = 0;
+static uint8_t display_maker = 0;
+static uint8_t cg_ink_color = 0;
+static uint8_t hw_version = 0;
+static uint16_t project_id = 0;
+static uint8_t cg_maker = 0;
+static uint8_t reservation_byte = 0;
 
 /*******************************************************
 Description:
