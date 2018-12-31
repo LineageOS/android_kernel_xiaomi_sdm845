ecf64fe016e0c6b753153edd478570f001b233c3
DevriesL <therkduan@gmail.com>
2018-12-31 21:42:21 +0800

    drm: msm: dsi-staging: add get_main_display
    Change-Id: Id670875adc18659521c424a716cf0ef194f9627f

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.h
@@ -671,4 +671,6 @@ int dsi_display_cont_splash_config(void *display);
 int dsi_display_get_panel_vfp(void *display,
 	int h_active, int v_active);
 
+struct dsi_display *get_main_display(void);
+
 #endif /* _DSI_DISPLAY_H_ */
