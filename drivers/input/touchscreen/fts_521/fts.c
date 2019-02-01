b05430e45df881f95c50c27ba6d8310e76c512be
Biswajit Dash <bisdash@google.com>
2019-02-01 15:39:09 -0800

    touchscreen: fts_521: Fix potential fts global data corruption.
    some of the driver sysfs store interface callbacks were not checking for
    sscanf() return before storing the parsed data in fts global driver
    structure. This could potentially corrupt the global data by writing
    uninitialized stack variables if sscanf() fails to parse the input buffer.
    
    Bug: 120211363
    Bug: 120211290
    Bug: 120211138
    Bug: 120211749
    Bug: 120211824
    
    Change-Id: I13998ab3270913dbc4b3a160696ab87405d6f25d
    Signed-off-by: Biswajit Dash <bisdash@google.com>

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -514,17 +514,24 @@ static ssize_t fts_grip_mode_store(struct device *dev,
 				   const char *buf, size_t count)
 {
 	char *p = (char *)buf;
-	unsigned int temp;
+	unsigned int temp = FEAT_DISABLE;
 	int res;
 	struct fts_ts_info *info = dev_get_drvdata(dev);
+	ssize_t retval = count;
 
 /*in case of a different elaboration of the input, just modify this initial part of the code according to customer needs*/
 	if ((count + 1) / 3 != 1) {
 		pr_err("%s: Number of bytes of parameter wrong! %zu != 1 byte\n",
 			__func__, (count + 1) / 3);
 	} else {
-		sscanf(p, "%02X ", &temp);
-		p += 3;
+		res = sscanf(p, "%02X ", &temp);
+		if ((res != 1) || (temp > FEAT_ENABLE)) {
+			pr_err("%s: Missing or invalid grip mode(%u)\n",
+				__func__, temp);
+			retval = -EINVAL;
+			goto exit;
+		}
+
 /*
 *this is a standard code that should be always used when a feature is enabled!
 *first step : check if the wanted feature can be enabled
@@ -541,7 +548,8 @@ static ssize_t fts_grip_mode_store(struct device *dev,
 			}
 		}
 	}
-	return count;
+exit:
+	return retval;
 }
 #endif
 
@@ -578,17 +586,24 @@ static ssize_t fts_charger_mode_store(struct device *dev,
 				      const char *buf, size_t count)
 {
 	char *p = (char *)buf;
-	unsigned int temp;
+	unsigned int temp = FEAT_DISABLE;
 	int res;
 	struct fts_ts_info *info = dev_get_drvdata(dev);
+	ssize_t retval = count;
 
 /*in case of a different elaboration of the input, just modify this initial part of the code according to customer needs*/
 	if ((count + 1) / 3 != 1) {
 		pr_err("%s: Number of bytes of parameter wrong! %zu != 1 byte\n",
 			__func__, (count + 1) / 3);
+		retval = -EINVAL;
 	} else {
-		sscanf(p, "%02X ", &temp);
-		p += 3;
+		res = sscanf(p, "%02X ", &temp);
+		if ((res != 1) || (temp > FEAT_ENABLE)) {
+			pr_err("%s: Missing or invalid charger mode (%u)\n",
+				__func__, temp);
+			retval = -EINVAL;
+			goto exit;
+		}
 /*
 *this is a standard code that should be always used when a feature is enabled!
 *first step : check if the wanted feature can be enabled
@@ -605,7 +620,8 @@ static ssize_t fts_charger_mode_store(struct device *dev,
 			}
 		}
 	}
-	return count;
+exit:
+	return retval;
 }
 #endif
 
@@ -642,17 +658,24 @@ static ssize_t fts_glove_mode_store(struct device *dev,
 				    const char *buf, size_t count)
 {
 	char *p = (char *)buf;
-	unsigned int temp;
+	unsigned int temp = FEAT_DISABLE;
 	int res;
 	struct fts_ts_info *info = dev_get_drvdata(dev);
+	ssize_t retval = count;
 
 /*in case of a different elaboration of the input, just modify this initial part of the code according to customer needs*/
 	if ((count + 1) / 3 != 1) {
 		pr_err("%s: Number of bytes of parameter wrong! %zu != 1 byte\n",
 			__func__, (count + 1) / 3);
+		retval = -EINVAL;
 	} else {
-		sscanf(p, "%02X ", &temp);
-		p += 3;
+		res = sscanf(p, "%02X ", &temp);
+		if ((res != 1) || (temp > FEAT_ENABLE)) {
+			pr_err("%s: Missing or invalid glove mode(%u)\n",
+				__func__, temp);
+			retval = -EINVAL;
+			goto exit;
+		}
 /*
 *this is a standard code that should be always used when a feature is enabled!
 *first step : check if the wanted feature can be enabled
@@ -670,7 +693,8 @@ static ssize_t fts_glove_mode_store(struct device *dev,
 		}
 	}
 
-	return count;
+exit:
+	return retval;
 }
 #endif
 
@@ -709,16 +733,24 @@ static ssize_t fts_cover_mode_store(struct device *dev,
 				    const char *buf, size_t count)
 {
 	char *p = (char *)buf;
-	unsigned int temp;
+	unsigned int temp = FEAT_DISABLE;
 	int res;
 	struct fts_ts_info *info = dev_get_drvdata(dev);
+	ssize_t retval = count;
 
 /*in case of a different elaboration of the input, just modify this initial part of the code according to customer needs*/
 	if ((count + 1) / 3 != 1) {
 		pr_err("%s: Number of bytes of parameter wrong! %zu != 1 byte\n",
 			__func__, (count + 1) / 3);
 	} else {
-		sscanf(p, "%02X ", &temp);
+		res = sscanf(p, "%02X ", &temp);
+		if ((res != 1) || (temp > FEAT_ENABLE)) {
+			pr_err("%s: Missing or invalid cover mode(%u)\n",
+				__func__, temp);
+			retval = -EINVAL;
+			goto exit;
+		}
+
 		p += 3;
 /*
 *this is a standard code that should be always used when a feature is enabled!
@@ -737,7 +769,8 @@ static ssize_t fts_cover_mode_store(struct device *dev,
 		}
 	}
 
-	return count;
+exit:
+	return retval;
 }
 #endif
 
@@ -774,20 +807,29 @@ static ssize_t fts_stylus_mode_store(struct device *dev,
 				     const char *buf, size_t count)
 {
 	char *p = (char *)buf;
-	unsigned int temp;
+	unsigned int temp = FEAT_DISABLE;
+	int res;
 	struct fts_ts_info *info = dev_get_drvdata(dev);
+	ssize_t retval = count;
 
 /*in case of a different elaboration of the input, just modify this initial part of the code according to customer needs*/
 	if ((count + 1) / 3 != 1) {
 		pr_err("%s: Number of bytes of parameter wrong! %zu != 1 byte\n",
 			__func__, (count + 1) / 3);
 	} else {
-		sscanf(p, "%02X ", &temp);
-		p += 3;
+		res = sscanf(p, "%02X ", &temp);
+		if ((res != 1) || (temp > FEAT_ENABLE)) {
+			pr_err("%s: Missing or invalid stylus mode(%u)\n",
+				__func__, temp);
+			retval = -EINVAL;
+			goto exit;
+		}
+
 		info->stylus_enabled = temp;
 
 	}
-	return count;
+exit:
+	return retval;
 }
 #endif
 
@@ -860,8 +902,9 @@ static ssize_t fts_gesture_mask_store(struct device *dev,
 				      const char *buf, size_t count)
 {
 	char *p = (char *)buf;
-	int n;
-	unsigned int temp;
+	int n, res;
+	unsigned int temp = 0;
+	ssize_t retval = count;
 
 	if ((count + 1) / 3 > GESTURE_MASK_SIZE + 1) {
 		pr_err("fts_gesture_mask_store: Number of bytes of parameter wrong! %zu > (enable/disable + %d )\n",
@@ -870,14 +913,21 @@ static ssize_t fts_gesture_mask_store(struct device *dev,
 	} else {
 		mask[0] = ((count + 1) / 3) - 1;
 		for (n = 1; n <= (count + 1) / 3; n++) {
-			sscanf(p, "%02X ", &temp);
+			res = sscanf(p, "%02X ", &temp);
+			if (res != 1) {
+				pr_err("%s: Invalid input\n", __func__);
+				retval = -EINVAL;
+				goto exit;
+			}
+
 			p += 3;
 			mask[n] = (u8) temp;
 			pr_info("mask[%d] = %02X\n", n, mask[n]);
 		}
 	}
 
-	return count;
+exit:
+	return retval;
 }
 
 #else
@@ -915,9 +965,10 @@ static ssize_t fts_gesture_mask_store(struct device *dev,
 {
 	char *p = (char *)buf;
 	int n;
-	unsigned int temp;
+	unsigned int temp = 0;
 	int res;
 	struct fts_ts_info *info = dev_get_drvdata(dev);
+	ssize_t retval = count;
 
 	if ((count + 1) / 3 < 2 || (count + 1) / 3 > GESTURE_MASK_SIZE + 1) {
 		pr_err("fts_gesture_mask_store: Number of bytes of parameter wrong! %d < or > (enable/disable + at least one gestureID or max %d bytes)\n",
@@ -926,11 +977,25 @@ static ssize_t fts_gesture_mask_store(struct device *dev,
 	} else {
 		memset(mask, 0, GESTURE_MASK_SIZE + 2);
 		mask[0] = ((count + 1) / 3) - 1;
-		sscanf(p, "%02X ", &temp);
+		res = sscanf(p, "%02X ", &temp);
+		if (res != 1) {
+			pr_err("%s: Invalid input(%u)\n",__func__, temp);
+			mask[0] = 0;
+			retval = -EINVAL;
+			goto bad_param;
+		}
+
 		p += 3;
 		mask[1] = (u8) temp;
 		for (n = 1; n < (count + 1) / 3; n++) {
-			sscanf(p, "%02X ", &temp);
+			res = sscanf(p, "%02X ", &temp);
+			if (res != 1) {
+				pr_err("%s: Invalid input\n", __func__);
+				mask[0] = 0;
+				retval = -EINVAL;
+				goto bad_param;
+			}
+
 			p += 3;
 			fromIDtoMask((u8) temp, &mask[2], GESTURE_MASK_SIZE);
 
@@ -942,22 +1007,21 @@ static ssize_t fts_gesture_mask_store(struct device *dev,
 
 	}
 
+bad_param;
 	if (mask[0] == 0) {
 		res = ERROR_OP_NOT_ALLOW;
 		pr_err("%s: Call before echo enable/disable xx xx .... > gesture_mask with a correct number of parameters! ERROR %08X\n",
 			__func__, res);
-	} else {
 
-		if (mask[1] == FEAT_ENABLE || mask[1] == FEAT_DISABLE)
-			res = updateGestureMask(&mask[2], mask[0], mask[1]);
-		else
-			res = ERROR_OP_NOT_ALLOW;
+		goto exit;
 
-		if (res < OK) {
-			pr_err("fts_gesture_mask_store: ERROR %08X\n", res);
-		}
+	if (mask[1] == FEAT_ENABLE || mask[1] == FEAT_DISABLE)
+		res = updateGestureMask(&mask[2], mask[0], mask[1]);
+	else
+		res = ERROR_OP_NOT_ALLOW;
 
-	}
+	if (res < OK)
+		pr_err("fts_gesture_mask_store: ERROR %08X\n", res);
 
 	res = check_feature_feasibility(info, FEAT_SEL_GESTURE);
 	temp = isAnyGestureActive();
@@ -966,7 +1030,8 @@ static ssize_t fts_gesture_mask_store(struct device *dev,
 	}
 	res = fts_mode_handler(info, 0);
 
-	return count;
+exit:
+	return retval;
 }
 
 #endif
