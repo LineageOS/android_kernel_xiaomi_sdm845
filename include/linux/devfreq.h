2fc08b072025cdf661dd353c06628d134612a843
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-03 13:01:52 -0800

    devfreq: Introduce devfreq boost driver
    This driver boosts enumerated devfreq devices upon input, and allows for
    boosting specific devfreq devices on other custom events. The boost
    frequencies for this driver should be set so that frame drops are
    near-zero at the boosted frequencies and power consumption is minimized
    at said frequencies. The goal of this driver is to provide an interface
    to achieve optimal device performance by requesting boosts on key
    events, such as when a frame is ready to rendered to the display.
    
    Currently, support is only present for boosting the cpubw devfreq
    device, but the driver is structured in a way that makes it easy to add
    support for new boostable devfreq devices in the future.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -182,6 +182,8 @@ struct devfreq {
 
 	unsigned long min_freq;
 	unsigned long max_freq;
+	bool is_boost_device;
+	bool max_boost;
 	bool stop_polling;
 
 	/* information for device frequency transition */
@@ -313,6 +315,9 @@ struct devfreq_passive_data {
 };
 #endif
 
+/* Caution: devfreq->lock must be locked before calling update_devfreq */
+extern int update_devfreq(struct devfreq *devfreq);
+
 #else /* !CONFIG_PM_DEVFREQ */
 static inline struct devfreq *devfreq_add_device(struct device *dev,
 					  struct devfreq_dev_profile *profile,
@@ -415,6 +420,11 @@ static inline struct devfreq *devfreq_get_devfreq_by_phandle(struct device *dev,
 }
 
 static inline int devfreq_update_stats(struct devfreq *df)
+{
+	return -EINVAL;
+}
+
+static inline int update_devfreq(struct devfreq *devfreq)
 {
 	return -EINVAL;
 }
---
+++ include/linux/devfreq_boost.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2018-2019 Sultan Alsawaf <sultan@kerneltoast.com>.
+ */
+#ifndef _DEVFREQ_BOOST_H_
+#define _DEVFREQ_BOOST_H_
+
+#include <linux/devfreq.h>
+
+enum df_device {
+	DEVFREQ_MSM_CPUBW,
+	DEVFREQ_MAX
+};
+
+#ifdef CONFIG_DEVFREQ_BOOST
+void devfreq_boost_kick(enum df_device device);
+void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms);
+void devfreq_register_boost_device(enum df_device device, struct devfreq *df);
+#else
+static inline
+void devfreq_boost_kick(enum df_device device)
+{
+}
+static inline
+void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
+{
+}
+static inline
+void devfreq_register_boost_device(enum df_device device, struct devfreq *df)
+{
+}
+#endif
+
+#endif /* _DEVFREQ_BOOST_H_ */
