432b0fb76ebb862e29aaf9c41420389d6ab30a70
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2019-03-08 00:33:50 +0530

    qcacld-3.0: Advertise MDIE in FT-FILS authentication frame
    IEEE802.11ai standard mandates Mobility domain IE to be
    advertised in authentication frames for FT-FILS connection.
    With FT-FILS akm suite connection, initial association to the
    AP happens with full EAP-8021x handshake. When association to
    the AP happens second time, FILS connection with auth algo 4 and
    ERP keys exchange happens. During this FILS association, host
    driver doesn't send MDIE in the authentication request frame.
    This will result in authentication rejection with failure code:
    INVALID_MDIE.
    
    Advertise MDIE in authentication frame during FT-FILS initial
    mobility domain association.
    
    Also mark the connection as 11r connection in PE session when
    the AKM suite is FT-FILS SHA-256 or FT-FILS SHA 384.
    
    Change-Id: I4dbbd9b0ef67ef3a6eb3b8afaaaa22be260d7b3e
    CRs-Fixed: 2411728

--- core/mac/src/pe/lim/lim_send_management_frames.c
+++ core/mac/src/pe/lim/lim_send_management_frames.c
@@ -2330,6 +2330,12 @@ lim_send_auth_mgmt_frame(tpAniSirGlobal mac_ctx,
 				frame_len += (2 + SIR_MDIE_SIZE);
 			}
 		}
+
+		/* include MDIE in FILS authentication frame */
+		if (session->pLimJoinReq->is11Rconnection &&
+		    auth_frame->authAlgoNumber == SIR_FILS_SK_WITHOUT_PFS &&
+		    session->pLimJoinReq->bssDescription.mdiePresent)
+			frame_len += (2 + SIR_MDIE_SIZE);
 		break;
 
 	case SIR_MAC_AUTH_FRAME_2:
@@ -2507,10 +2513,11 @@ alloc_packet:
 						pbssDescription->mdie[0],
 					SIR_MDIE_SIZE);
 			}
-		} else if (auth_frame->authAlgoNumber ==
-				SIR_FILS_SK_WITHOUT_PFS) {
-			/* TODO MDIE */
-			pe_debug("appending fils Auth data");
+		} else if ((auth_frame->authAlgoNumber ==
+					SIR_FILS_SK_WITHOUT_PFS) &&
+			   (auth_frame->authTransactionSeqNumber ==
+						SIR_MAC_AUTH_FRAME_1)) {
+			pe_debug("FILS: appending fils Auth data");
 			lim_add_fils_data_to_auth_frame(session, body);
 		}
 
