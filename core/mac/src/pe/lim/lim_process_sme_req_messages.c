eaf95a195883f5a1d0f263d0ca3474e4fff741a7
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2019-03-12 18:06:01 +0530

    qcacld-3.0: Derive PMK-R0 and PMK-R1NAME for FT-FILS
    Generate PMK-R0 and PMK-R1NAME for FT-FILS connection and send it
    over assoc request frame.
    
    According to 802.11-2016 standard:
    PMK-R0 is derived as:
    R0-Key-Data = KDF-Hash-Length(XXKey, "FT-R0", SSIDlength ||
    				SSID || MDID || R0KHlength ||
    				R0KH-ID || S0KH-ID)
    PMK-R0 = L(R0-Key-Data, 0, Q)
    PMKR0Name = Truncate-128(SHA-256("FT-R0N" || PMK-R0Name-Salt))
    
    PMK-R1 is derived as:
    PMK-R1 = KDF-Hash-Length(PMK-R0, "FT-R1", R1KH-ID || S1KH-ID)
    
    PMK-R1Name is derived as:
    PMKR1Name = Truncate-128(SHA-256(“FT-R1N” || PMKR0Name ||
    			R1KH-ID || S1KH-ID))
    
    Call qdf_get_hash() function to generate a sha-256 or sha-384
    hash. Use qdf_get_hash() with hmac(sha-256) or hmac(sha-384)
    to generate hmac_hash.
    
    Set PMKID count as 1 in RSN IE and copy the PMKR1-name
    generated to the PMKID list. This PMKR1 should be the only
    entry in PMKID list in assoc request frame sent by the driver
    during FT-FILS initial mobility domain FILS connection.
    
    Change-Id: Ic634aebbe42a58b92f871cf3258c62f7541d161a
    CRs-Fixed: 2414719

--- core/mac/src/pe/lim/lim_process_sme_req_messages.c
+++ core/mac/src/pe/lim/lim_process_sme_req_messages.c
@@ -1514,7 +1514,7 @@ __lim_process_sme_join_req(tpAniSirGlobal mac_ctx, uint32_t *msg_buf)
 		session->txLdpcIniFeatureEnabled =
 			sme_join_req->txLdpcIniFeatureEnabled;
 
-		lim_update_fils_config(session, sme_join_req);
+		lim_update_fils_config(mac_ctx, session, sme_join_req);
 		lim_update_sae_config(session, sme_join_req);
 		if (session->bssType == eSIR_INFRASTRUCTURE_MODE) {
 			session->limSystemRole = eLIM_STA_ROLE;
