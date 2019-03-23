a8d7501eb797ce514442c0bd34832135fc9b001c
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-23 14:09:14 -0700

    kernel: Boost whenever a zygote-forked process becomes a top app
    Boost to the max for 500 ms whenever the top app changes, which
    improves app launch speeds and addresses jitter when switching between
    apps. A check to make sure that the top-app's parent is zygote ensures
    that a user-facing app is indeed what's added to the top app task group,
    since app processes are forked from zygote.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -122,5 +122,6 @@ extern int prepare_bprm_creds(struct linux_binprm *bprm);
 extern void install_exec_creds(struct linux_binprm *bprm);
 extern void set_binfmt(struct linux_binfmt *new);
 extern ssize_t read_code(struct file *, unsigned long, loff_t, size_t);
+extern bool is_zygote_pid(pid_t pid);
 
 #endif /* _LINUX_BINFMTS_H */
