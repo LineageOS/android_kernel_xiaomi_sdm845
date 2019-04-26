99906ce5c5861476ca6ec9d1f43f2eed453449e7
Park Ju Hyung <qkrwngud825@gmail.com>
2019-04-27 01:17:18 +0900

    msm: vidc: avoid VLA
    Change-Id: If73210c9395b0e231c7b35f5ab10b07d41c75add
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: kanonifyX <kanonify01@gmail.com>
    Signed-off-by: awkoo <awkooox@gmail.com>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -142,7 +142,7 @@ static void __dump_packet(u8 *packet, enum vidc_msg_prio log_level)
 	 * row must contain enough for 0xdeadbaad * 8 to be converted into
 	 * "de ad ba ab " * 8 + '\0'
 	 */
-	char row[3 * row_size];
+	char row[3 * 32];
 
 	for (c = 0; c * row_size < packet_size; ++c) {
 		int bytes_to_read = ((c + 1) * row_size > packet_size) ?
