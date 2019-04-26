f62306f285e1ce74347fd275c141c76fedd7da07
Bala Venkatesh <bjavvaji@codeaurora.org>
2019-04-26 16:30:25 +0530

    qcacld-3.0: Don't allow update peer command if peer not found
    Do not allow tdls peer update command if tdls peer is not
    found in lim. If peer assoc command is given to FW
    without peer create, FW will assert.
    
    Remove tdls peers in tdls component in case of roaming as
    peers are deleted in lim as part of roaming.
    
    Change-Id: Ie227da682e4d30c9b583881f7e2eaafe91826476
    CRs-Fixed: 2441951

--- core/mac/src/pe/lim/lim_process_tdls.c
+++ core/mac/src/pe/lim/lim_process_tdls.c
@@ -2580,7 +2580,10 @@ static QDF_STATUS lim_tdls_setup_add_sta(tpAniSirGlobal pMac,
 	if (pStaDs && pAddStaReq->tdlsAddOper == TDLS_OPER_ADD) {
 		pe_err("TDLS entry for peer: "MAC_ADDRESS_STR " already exist, cannot add new entry",
 			MAC_ADDR_ARRAY(pAddStaReq->peermac.bytes));
-			return QDF_STATUS_E_FAILURE;
+	}
+	if (!pStaDs && pAddStaReq->tdlsAddOper == TDLS_OPER_UPDATE) {
+		pe_err("TDLS update peer is given without peer creation");
+		return QDF_STATUS_E_FAILURE;
 	}
 
 	if (pStaDs && pStaDs->staType != STA_ENTRY_TDLS_PEER) {
@@ -3203,6 +3206,9 @@ QDF_STATUS lim_delete_tdls_peers(tpAniSirGlobal mac_ctx,
 
 	lim_check_aid_and_delete_peer(mac_ctx, session_entry);
 
+	tgt_tdls_delete_all_peers_indication(mac_ctx->psoc,
+					     session_entry->smeSessionId);
+
 	if (lim_is_roam_synch_in_progress(session_entry))
 		return QDF_STATUS_SUCCESS;
 	/* In case of CSA, Only peers in lim and TDLS component
@@ -3215,9 +3221,6 @@ QDF_STATUS lim_delete_tdls_peers(tpAniSirGlobal mac_ctx,
 						    session_entry->
 						    smeSessionId);
 	}
-
-	tgt_tdls_delete_all_peers_indication(mac_ctx->psoc,
-					     session_entry->smeSessionId);
 
 	/* reset the set_state_disable flag */
 	session_entry->tdls_send_set_state_disable = true;
