8170f2ea65a09f98683cd1aa784d13d5c9c8d32e
Demon000 <demonsingur@gmail.com>
2019-05-20 22:27:42 +0200

    halls: rewrite to remove userspace dependency
    Change-Id: Iac8f4dcebefe43d1a69dea2f5517e9b2432fac1b

--- drivers/halls/halls.c
+++ drivers/halls/halls.c
@@ -1,308 +1,174 @@
 #include <linux/gpio.h>
-#include <linux/fdtable.h>
-#include <linux/file.h>
-#include <linux/miscdevice.h>
 #include <linux/module.h>
-#include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/irq.h>
 #include <linux/interrupt.h>
-#include <linux/semaphore.h>
 #include <linux/workqueue.h>
 #include <linux/pm.h>
+#include <linux/input.h>
 
-#include <asm/current.h>
+#define HALLS_DEVICE_NAME "halls"
+#define HALLS_GPIO_FIRST 49
+#define HALLS_GPIO_SECOND 113
 
-#define HALLS_LOG printk
+enum {
+	KEYCODE_SLIDER_UP = 594,
+	KEYCODE_SLIDER_DOWN,
+};
 
-#define LOGTAG "halls "
-#define DEF_HALLS_DEVICE_NAME "halls"
+enum {
+	STATE_SLIDER_UP = 0,
+	STATE_SLIDER_DOWN,
+	STATE_SLIDER_SLIDING,
+};
 
-#define DEF_HALLS_IOCTRL_READ_HALL_1 1000
-#define DEF_HALLS_IOCTRL_READ_HALL_2 2000
-#define DEF_HALLS_IOCTRL_FLUSH 3000
+struct halls_data {
+	struct delayed_work notify_work;
+	struct wakeup_source *wakelock;
+	struct input_dev *input;
 
-#define DEF_HALLS_IOCTRL_STEUP 4000
-#define DEF_HALLS_IOCTRL_RELEASE 5000
-
-#define DEF_HALL_IRQ_1_FIRED "h-1"
-#define DEF_HALL_IRQ_2_FIRED "h-2"
-#define DEF_HALL_FLUSH  "flu"
-
-#define DEF_HALL_GPIO_1 49
-#define DEF_HALL_GPIO_2 113
+	int first_gpio;
+	int second_gpio;
+};
 
 extern int elliptic_set_hall_state(int state);
+static void halls_send_input(struct halls_data *hdata, int state) {
+	int keycode;
 
-static int g_gpio_1_irq;
-static int g_gpio_2_irq;
-static int g_in_firedirq;
-static int g_in_end;
-static int g_in_flush;
+	if (state == STATE_SLIDER_UP)
+		keycode = KEYCODE_SLIDER_UP;
+	else if (state == STATE_SLIDER_DOWN)
+		keycode = KEYCODE_SLIDER_DOWN;
+	else
+		return;
 
-static int g_in_opencounter;
-
-struct _hall_device {
-	struct miscdevice miscdev;
-	struct delayed_work notify_work;
-	struct wakeup_source *hall_wakelock;
-	int hall_status;
-};
-
-struct _hall_device *g_st_halls;
-struct semaphore g_st_semhallirq;
-
-static void hall_notify_work_func(struct work_struct *work)
-{
-	int iRet1 = 0, iRet2 = 0;
-	iRet1 = gpio_get_value_cansleep(DEF_HALL_GPIO_1);
-	iRet2 = gpio_get_value_cansleep(DEF_HALL_GPIO_2);
-	HALLS_LOG(LOGTAG "g_in_firedirq %d, g_gpio_1_irq %d, g_gpio_2_irq %d",
-		  g_in_firedirq, g_gpio_1_irq, g_gpio_2_irq);
-	HALLS_LOG(LOGTAG " iRet1 %d	iRet2 %d", iRet1, iRet2);
-	if (iRet1 == 0 && iRet2 == 1) {
-		g_st_halls->hall_status = 1;
-	} else if (iRet1 == 1 && iRet2 == 0) {
-		g_st_halls->hall_status = 0;
-	} else {
-		g_st_halls->hall_status = 2;
-	}
-
-	HALLS_LOG(LOGTAG "hall_notify_work_func notify audio hall_status: %d!",
-		  g_st_halls->hall_status);
-	elliptic_set_hall_state(g_st_halls->hall_status);
+	input_report_key(hdata->input, keycode, 1);
+	input_sync(hdata->input);
+	input_report_key(hdata->input, keycode, 0);
+	input_sync(hdata->input);
 }
 
-static irqreturn_t halls_irq_handler(int irqno, void *dev_id)
-{
-	HALLS_LOG(LOGTAG "Entered halls_irq_handler irqno %d\n", irqno);
-	__pm_wakeup_event(g_st_halls->hall_wakelock, 20);
-	g_in_firedirq = irqno;
-	up(&g_st_semhallirq);
-	schedule_delayed_work(&g_st_halls->notify_work, 0);
+static void halls_notify_work_func(struct work_struct *work) {
+	struct halls_data *hdata = container_of(to_delayed_work(work),
+			struct halls_data, notify_work);
+	int first_value, second_value;
+	int state;
+
+	first_value = gpio_get_value_cansleep(hdata->first_gpio);
+	second_value = gpio_get_value_cansleep(hdata->second_gpio);
+
+	if (first_value == 1 && second_value == 0)
+		state = STATE_SLIDER_UP;
+	else if (first_value == 0 && second_value == 1)
+		state = STATE_SLIDER_DOWN;
+	else
+		state = STATE_SLIDER_SLIDING;
+
+	elliptic_set_hall_state(state);
+	halls_send_input(hdata, state);
+}
+
+static irqreturn_t halls_irq_handler(int irqno, void *dev_id) {
+	struct halls_data *hdata = dev_id;
+	__pm_wakeup_event(hdata->wakelock, 20);
+	schedule_delayed_work(&hdata->notify_work, 0);
 	return 0;
 }
 
-static int halls_configurate_input(int in_GPIO, const char *ch_mark)
-{
-	int iRet = 0;
+static int halls_configure_irq(struct halls_data *hdata, int gpio, const char *label) {
+	int ret, irq;
 
-	iRet = gpio_request(in_GPIO, ch_mark);
-	if (iRet)
-		pr_err("%d is being used!\n", in_GPIO);
-
-	iRet = gpio_direction_input(in_GPIO);
-	if (iRet)
-		pr_err("Setting hall gpio direction failed! iRet %d\n", iRet);
-
-	return iRet;
-}
-
-static int halls_configurate_irq(int in_GPIO,
-				 const char *ch_mark, void *arg, int *out_irq)
-{
-	int iRet = 0, in_IRQ = 0;
-
-	in_IRQ = gpio_to_irq(in_GPIO);
-	*out_irq = in_IRQ;
-	HALLS_LOG(LOGTAG "in_GPIO %d, IRQ %d\n", in_GPIO, in_IRQ);
-	iRet = request_irq(in_IRQ, halls_irq_handler,
-			   IRQF_TRIGGER_RISING
-			   | IRQF_TRIGGER_FALLING | IRQF_ONESHOT, ch_mark, arg);
-	if (iRet)
-		pr_err("request_irq failed! iRet %d\n", iRet);
-	irq_set_irq_wake(in_IRQ, 1);
-
-	HALLS_LOG(LOGTAG "request_irq return %d\n", iRet);
-	return iRet;
-}
-
-static ssize_t halls_read(struct file *file,
-			  char __user *buf, size_t count, loff_t *ppos)
-{
-	int iRead = 0;
-
-	HALLS_LOG(LOGTAG "Entereed halls_read  count  %d", (int)count);
-	if (down_interruptible(&g_st_semhallirq)) {
-		HALLS_LOG(LOGTAG "down_interruptible failed!");
+	ret = gpio_request(gpio, label);
+	if (ret) {
+		pr_err("%s: failed to request GPIO %d\n", __func__, gpio);
+		return ret;
 	}
-	if (g_in_end == 1) {
-		HALLS_LOG(LOGTAG "Ending... ");
-		return -EPERM;
+
+	ret = gpio_direction_input(gpio);
+	if (ret) {
+		pr_err("%s: failed to set GPIO %d direction to input\n", __func__, gpio);
+		return ret;
 	}
-	HALLS_LOG(LOGTAG "g_in_firedirq %d, g_gpio_1_irq %d, g_gpio_2_irq %d",
-		  g_in_firedirq, g_gpio_1_irq, g_gpio_2_irq);
-	if (g_in_flush == 1) {
-		g_in_flush = 0;
-		iRead =
-		    copy_to_user(buf, DEF_HALL_FLUSH, strlen(DEF_HALL_FLUSH));
-		HALLS_LOG(LOGTAG "End of copy_to_user flash... ");
-	} else if (g_in_firedirq == g_gpio_1_irq) {
-		iRead = copy_to_user(buf, DEF_HALL_IRQ_1_FIRED,
-				     strlen(DEF_HALL_IRQ_1_FIRED));
-		HALLS_LOG(LOGTAG "End of copy_to_user 1... ");
-	} else if (g_in_firedirq == g_gpio_2_irq) {
-		iRead = copy_to_user(buf, DEF_HALL_IRQ_2_FIRED,
-				     strlen(DEF_HALL_IRQ_2_FIRED));
-		HALLS_LOG(LOGTAG "End of copy_to_user 2... ");
+
+	irq = gpio_to_irq(gpio);
+	ret = request_irq(irq, halls_irq_handler,
+			IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING
+			| IRQF_ONESHOT, label, hdata);
+	if (ret) {
+		pr_err("%s: failed to configure GPIO %d IRQ\n", __func__, gpio);
+		return ret;
 	}
-	return iRead;
-}
 
-long halls_in_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
-{
-	int iRet = 0;
-	void *p_ReadValue = (void *)arg;
-	char ch_data[10] = { 0 };
+	irq_set_irq_wake(irq, 1);
 
-	HALLS_LOG(LOGTAG "Enter halls_in_ioctl  cmd %d", cmd);
-	switch (cmd) {
-	case DEF_HALLS_IOCTRL_READ_HALL_1:
-		{
-			iRet = gpio_get_value_cansleep(DEF_HALL_GPIO_1);
-			HALLS_LOG(LOGTAG "Read HALL_1 %d", iRet);
-			snprintf(ch_data, sizeof(ch_data), "%d", iRet);
-			iRet =
-			    copy_to_user(p_ReadValue, ch_data, strlen(ch_data));
-			break;
-		}
-	case DEF_HALLS_IOCTRL_READ_HALL_2:
-		{
-			iRet = gpio_get_value_cansleep(DEF_HALL_GPIO_2);
-			HALLS_LOG(LOGTAG "Read HALL_2 %d", iRet);
-			snprintf(ch_data, sizeof(ch_data), "%d", iRet);
-			iRet =
-			    copy_to_user(p_ReadValue, ch_data, strlen(ch_data));
-			break;
-		}
-	case DEF_HALLS_IOCTRL_FLUSH:
-		{
-			g_in_flush = 1;
-			up(&g_st_semhallirq);
-			schedule_delayed_work(&g_st_halls->notify_work, 0);
-			break;
-		}
-	case DEF_HALLS_IOCTRL_STEUP:
-		{
-			if (g_in_opencounter == 0) {
-				g_in_end = 0;
-				sema_init(&g_st_semhallirq, 0);
-				iRet =
-				    halls_configurate_irq(DEF_HALL_GPIO_1,
-							  "hall-1",
-							  (void *)g_st_halls,
-							  &g_gpio_1_irq);
-				if (iRet)
-					pr_err
-					    ("halls_configurate_irq failed!%d, iRet %d\n",
-					     DEF_HALL_GPIO_1, iRet);
-				g_in_firedirq = g_gpio_1_irq;
-
-				iRet =
-				    halls_configurate_irq(DEF_HALL_GPIO_2,
-							  "hall-2",
-							  (void *)g_st_halls,
-							  &g_gpio_2_irq);
-				if (iRet)
-					pr_err
-					    ("halls_configurate_irq failed! %d iRet %d\n",
-					     DEF_HALL_GPIO_2, iRet);
-
-				HALLS_LOG(LOGTAG
-					  "g_gpio_2_irq %d, g_gpio_2_irq %d\n",
-					  g_gpio_1_irq, g_gpio_2_irq);
-				g_in_opencounter = 1;
-			}
-			break;
-		}
-	case DEF_HALLS_IOCTRL_RELEASE:
-		{
-			g_in_end = 1;
-			up(&g_st_semhallirq);
-			if (g_gpio_1_irq > 0)
-				free_irq(g_gpio_1_irq, g_st_halls);
-
-			if (g_gpio_2_irq > 0)
-				free_irq(g_gpio_2_irq, g_st_halls);
-			g_in_opencounter = 0;
-			break;
-		}
-	default:
-		iRet = -1;
-	}
-	HALLS_LOG(LOGTAG "read value cmd %d, iRet %d", cmd, iRet);
-	return iRet;
-}
-
-int halls_flush(struct file *file, fl_owner_t id)
-{
-	HALLS_LOG(LOGTAG " Entered halls_flush \n");
 	return 0;
 }
 
-int halls_open(struct inode *node, struct file *file)
-{
-	int iRet = 0;
-
-	return iRet;
-}
-
-static const struct file_operations halls_fops = {
-	.owner = THIS_MODULE,
-	.read = halls_read,
-	.unlocked_ioctl = halls_in_ioctl,
-	.flush = halls_flush,
-	.open = halls_open
-};
-
-static int __init halls_init(void)
-{
+static int __init halls_init(void) {
+	struct halls_data *hdata;
 	int ret;
-	g_st_halls = kzalloc(sizeof(struct _hall_device), GFP_KERNEL);
-	if (!g_st_halls)
-		return -ENOMEM;
-	g_st_halls->miscdev.fops = &halls_fops;
-	g_st_halls->miscdev.minor = MISC_DYNAMIC_MINOR;
-	g_st_halls->miscdev.name = DEF_HALLS_DEVICE_NAME;
-	ret = misc_register(&g_st_halls->miscdev);
-	if (ret < 0) {
-		kfree(g_st_halls);
-		return ret;
+
+	hdata = kzalloc(sizeof(*hdata), GFP_KERNEL);
+	if (!hdata) {
+		pr_err("%s: failed to allocate memory for driver data\n", __func__);
+		ret = -ENOMEM;
+		goto exit;
 	}
-	ret = halls_configurate_input(DEF_HALL_GPIO_1, "hall-1");
+
+	hdata->input = input_allocate_device();
+	if (!hdata->input) {
+		pr_err("%s: failed to allocate memory for input device\n", __func__);
+		ret = -ENOMEM;
+		goto free_halls;
+	}
+
+	hdata->input->name = HALLS_DEVICE_NAME;
+	input_set_drvdata(hdata->input, hdata);
+
+	set_bit(EV_KEY, hdata->input->evbit);
+	set_bit(KEYCODE_SLIDER_UP, hdata->input->keybit);
+	set_bit(KEYCODE_SLIDER_DOWN, hdata->input->keybit);
+
+	ret = input_register_device(hdata->input);
 	if (ret) {
-		pr_err("halls_configurate_input failed!%d, ret %d\n",
-		       DEF_HALL_GPIO_1, ret);
-		gpio_free(DEF_HALL_GPIO_1);
-		return ret;
+		pr_err("%s: failed to register input device\n", __func__);
+		goto free_input;
 	}
-	ret = halls_configurate_input(DEF_HALL_GPIO_2, "hall-2");
+
+	hdata->first_gpio = HALLS_GPIO_FIRST;
+	hdata->second_gpio = HALLS_GPIO_SECOND;
+
+	ret = halls_configure_irq(hdata, hdata->first_gpio, "halls-first");
 	if (ret) {
-		pr_err("halls_configurate_input failed! %d ret %d\n",
-		       DEF_HALL_GPIO_2, ret);
-		gpio_free(DEF_HALL_GPIO_2);
-		return ret;
+		goto unregister_input;
 	}
 
-	g_st_halls->hall_status = 0;
-	INIT_DELAYED_WORK(&g_st_halls->notify_work, hall_notify_work_func);
-
-	g_st_halls->hall_wakelock = wakeup_source_register("dual_hall_ws");
-	if (!g_st_halls->hall_wakelock) {
-		pr_err("g_st_halls wakeup_source_register failed!\n");
-		return ret;
+	ret = halls_configure_irq(hdata, hdata->second_gpio, "halls-second");
+	if (ret) {
+		goto unregister_input;
 	}
 
+	INIT_DELAYED_WORK(&hdata->notify_work, halls_notify_work_func);
+
+	hdata->wakelock = wakeup_source_register("halls-ws");
+	if (!hdata->wakelock) {
+		pr_err("%s: failed to register wakeup source\n", __func__);
+		ret = -EINVAL;
+		goto unregister_input;
+	}
+
+	return 0;
+
+unregister_input:
+	input_unregister_device(hdata->input);
+free_input:
+	input_free_device(hdata->input);
+free_halls:
+	kfree(hdata);
+exit:
 	return ret;
 }
 
 device_initcall(halls_init);
 
-static void __exit halls_exit(void)
-{
-	gpio_free(DEF_HALL_GPIO_1);
-	gpio_free(DEF_HALL_GPIO_2);
-}
-
-module_exit(halls_exit);
 MODULE_LICENSE("GPL v2");
