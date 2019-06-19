b57b68426e890bbc48dca5e473f507a2c1f2580b
hangtian <hangtian@codeaurora.org>
2019-06-19 12:17:23 +0800

    qcacld-3.0: Fix cfg80211_ops.set_tx_power invalid bssid issue
    Fix cfg80211_ops.set_tx_power invalid bssid issue. User could set
    txpower via cfg80211 interface.
    
    Change-Id: Ie103a45e426b920846b8e4d23c4995ce660dde70
    CRs-Fixed: 2468749

--- core/hdd/src/wlan_hdd_power.c
+++ core/hdd/src/wlan_hdd_power.c
@@ -2048,8 +2048,9 @@ static int __wlan_hdd_cfg80211_set_txpower(struct wiphy *wiphy,
 {
 	struct hdd_context *hdd_ctx = (struct hdd_context *) wiphy_priv(wiphy);
 	mac_handle_t mac_handle;
+	struct hdd_adapter *adapter = WLAN_HDD_GET_PRIV_PTR(wdev->netdev);
 	struct qdf_mac_addr bssid = QDF_MAC_ADDR_BCAST_INIT;
-	struct qdf_mac_addr selfMac = QDF_MAC_ADDR_BCAST_INIT;
+	struct qdf_mac_addr selfmac;
 	QDF_STATUS status;
 	int errno;
 	int dbm;
@@ -2068,6 +2069,20 @@ static int __wlan_hdd_cfg80211_set_txpower(struct wiphy *wiphy,
 	errno = wlan_hdd_validate_context(hdd_ctx);
 	if (errno)
 		return errno;
+
+	if (adapter->device_mode == QDF_SAP_MODE ||
+	    adapter->device_mode == QDF_P2P_GO_MODE) {
+		qdf_copy_macaddr(&bssid, &adapter->mac_addr);
+	} else {
+		struct hdd_station_ctx *sta_ctx =
+			WLAN_HDD_GET_STATION_CTX_PTR(adapter);
+
+		if (eConnectionState_Associated ==
+		    sta_ctx->conn_info.connState)
+			qdf_copy_macaddr(&bssid, &sta_ctx->conn_info.bssId);
+	}
+
+	qdf_copy_macaddr(&selfmac, &adapter->mac_addr);
 
 	mac_handle = hdd_ctx->mac_handle;
 
@@ -2097,7 +2112,7 @@ static int __wlan_hdd_cfg80211_set_txpower(struct wiphy *wiphy,
 	/* Fall through */
 	case NL80211_TX_POWER_LIMITED:
 	/* Limit TX power by the mBm parameter */
-		status = sme_set_max_tx_power(mac_handle, bssid, selfMac, dbm);
+		status = sme_set_max_tx_power(mac_handle, bssid, selfmac, dbm);
 		if (QDF_IS_STATUS_ERROR(status)) {
 			hdd_err("Setting maximum tx power failed, %d", status);
 			return -EIO;
