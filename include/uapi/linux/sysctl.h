200165d6adea298547bf9bac1a4e4e87922afd06
thanasis <thanasis>
2019-07-30 03:10:46 +0300

    include/uapi/linux/sysctl: Set VM_DIRTY_WB_CS = 3000

--- include/uapi/linux/sysctl.h
+++ include/uapi/linux/sysctl.h
@@ -173,7 +173,7 @@ enum
 	VM_PAGE_CLUSTER=10,	/* int: set number of pages to swap together */
 	VM_DIRTY_BACKGROUND=15,	/* dirty_background_ratio */
 	VM_DIRTY_RATIO=60,	/* dirty_ratio */
-	VM_DIRTY_WB_CS=13,	/* dirty_writeback_centisecs */
+	VM_DIRTY_WB_CS=3000,	/* dirty_writeback_centisecs */
 	VM_DIRTY_EXPIRE_CS=500,	/* dirty_expire_centisecs */
 	VM_NR_PDFLUSH_THREADS=15, /* nr_pdflush_threads */
 	VM_OVERCOMMIT_RATIO=16, /* percent of RAM to allow overcommit in */
