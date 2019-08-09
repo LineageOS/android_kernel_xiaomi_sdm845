9e95af1e54e37cd730f2298008912b8adf73f4fb
Danny Lin <danny@kdrag0n.dev>
2019-08-09 06:33:47 -0400

    cpuset: Move cpuset override values to Kconfig
    The new selection of config options are tailored for the cpusets found
    on stock sm8150 CAF ROMs. Default values were also taken from sm8150.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    [@0ctobot: Adapted for SDM845]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1787,14 +1787,13 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 {
 #ifdef CONFIG_CPUSET_ASSIST
 	static struct cs_target cs_targets[] = {
-		/* Little-only cpusets go first */
-		{ "background",		"0-1"},
-		{ "audio-app",		"0-3"},
-		{ "system-background", 	"0-3"},
-		{ "restricted",		"0-3"},
-		{ "top-app",		"0-7"},
-		{ "foreground",		"0-3,6-7"},
-		{ "camera-daemon",	"0-3,6-7"},
+		{ "audio-app",		CONFIG_CPUSET_AUDIO_APP },
+		{ "background",		CONFIG_CPUSET_BG },
+		{ "camera-daemon",	CONFIG_CPUSET_CAMERA },
+		{ "foreground",		CONFIG_CPUSET_FG },
+		{ "restricted",		CONFIG_CPUSET_RESTRICTED },
+		{ "system-background",	CONFIG_CPUSET_SYSTEM_BG },
+		{ "top-app",		CONFIG_CPUSET_TOP_APP },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
