19e5cd99907ac7abf07ce7ef8fd0b94bb1f2a234
Abhishek Ambure <aambure@codeaurora.org>
2019-09-19 16:50:29 +0530

    qcacld-3.0: Set stop bss in progress to sync stop bss operation
    To avoid race in stop bss while CSA and driver shutdown, set
    "stop_bss_in_progress" in SAP context before sending stop BSS
    request.
    
    Change-Id: I968b69c41bfd401f9ddad253894bffd805b19e7a
    CRs-fixed: 2530754

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -5893,6 +5893,10 @@ QDF_STATUS hdd_stop_adapter_ext(struct hdd_context *hdd_ctx,
 			QDF_STATUS status;
 			QDF_STATUS qdf_status;
 
+			/* Set the stop_bss_in_progress flag */
+			wlansap_set_stop_bss_inprogress(
+				WLAN_HDD_GET_SAP_CTX_PTR(adapter), true);
+
 			/* Stop Bss. */
 			status = wlansap_stop_bss(
 					WLAN_HDD_GET_SAP_CTX_PTR(adapter));
@@ -5915,6 +5919,8 @@ QDF_STATUS hdd_stop_adapter_ext(struct hdd_context *hdd_ctx,
 				hdd_err("failure in wlansap_stop_bss");
 			}
 			clear_bit(SOFTAP_BSS_STARTED, &adapter->event_flags);
+			wlansap_set_stop_bss_inprogress(
+				WLAN_HDD_GET_SAP_CTX_PTR(adapter), false);
 			policy_mgr_decr_session_set_pcl(hdd_ctx->psoc,
 						adapter->device_mode,
 						adapter->session_id);
