ff72c5091a246acdef037701078b0012aea48ebe
Hangtian Zhu <hangtian@codeaurora.org>
2019-09-23 15:40:40 +0800

    qcacld-3.0: Add macro protection for throughput level checking in rx cbk
    Add macro protection for throughput level checking in rx callback
    
    Change-Id: I43c4a0cabb770b6ef9538b90e07fd218b40f743b
    CRs-Fixed: 2522061

--- core/hdd/src/wlan_hdd_tx_rx.c
+++ core/hdd/src/wlan_hdd_tx_rx.c
@@ -2556,6 +2556,7 @@ void hdd_reset_tcp_delack(struct hdd_context *hdd_ctx)
  *
  * Return: True if vote level is high
  */
+#ifdef RX_PERFORMANCE
 bool hdd_is_current_high_throughput(struct hdd_context *hdd_ctx)
 {
 	if (hdd_ctx->cur_vote_level < PLD_BUS_WIDTH_HIGH)
@@ -2563,4 +2564,5 @@ bool hdd_is_current_high_throughput(struct hdd_context *hdd_ctx)
 	else
 		return true;
 }
+#endif
 #endif /* MSM_PLATFORM */
