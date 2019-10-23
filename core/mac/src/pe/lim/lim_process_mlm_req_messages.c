17e955692dacffc2ccde0e11b47c2be7975ae6e3
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2019-10-23 18:08:24 +0530

    qcacld-3.0: Fix null pointer dereference in lim_process_auth_retry_timer
    In lim, gpLimMlmAuthReq is freed in lim_restore_auth_state if
    auth failure timeout happens. gpLimMlmAuthReq is dereferenced in
    lim_process_auth_retry_timer. When there is a race where
    lim_restore_auth_state is called before lim_process_auth_retry_timer,
    then null pointer dereference of gpLimMlmAuthReq happens in
    lim_process_auth_retry_timer.
    
    Validate gpLimMlmAuthReq against null before accessing it.
    
    Change-Id: Ic1af10172aa98785c84165c4491c8bdc1b3e508f
    CRs-Fixed: 2550793

--- core/mac/src/pe/lim/lim_process_mlm_req_messages.c
+++ core/mac/src/pe/lim/lim_process_mlm_req_messages.c
@@ -2233,7 +2233,7 @@ static void lim_process_periodic_join_probe_req_timer(tpAniSirGlobal mac_ctx)
 static void lim_process_auth_retry_timer(tpAniSirGlobal mac_ctx)
 {
 	tpPESession session_entry;
-	tAniAuthType auth_type = mac_ctx->lim.gpLimMlmAuthReq->authType;
+	tAniAuthType auth_type;
 	tLimTimers *lim_timers = &mac_ctx->lim.limTimers;
 	uint16_t vdev_id =
 		lim_timers->g_lim_periodic_auth_retry_timer.sessionId;
@@ -2253,14 +2253,16 @@ static void lim_process_auth_retry_timer(tpAniSirGlobal mac_ctx)
 		 * Send the auth retry only in case we have received ack failure
 		 * else just restart the retry timer.
 		 */
-		if (LIM_AUTH_ACK_RCD_FAILURE == mac_ctx->auth_ack_status) {
+		if (LIM_AUTH_ACK_RCD_FAILURE == mac_ctx->auth_ack_status &&
+		    mac_ctx->lim.gpLimMlmAuthReq) {
+			auth_type = mac_ctx->lim.gpLimMlmAuthReq->authType;
+
 			/* Prepare & send Authentication frame */
 			if (session_entry->sae_pmk_cached &&
 			    auth_type == eSIR_AUTH_TYPE_SAE)
 				auth_frame.authAlgoNumber = eSIR_OPEN_SYSTEM;
 			else
-				auth_frame.authAlgoNumber = (uint8_t)
-					mac_ctx->lim.gpLimMlmAuthReq->authType;
+				auth_frame.authAlgoNumber = (uint8_t)auth_type;
 
 			auth_frame.authTransactionSeqNumber =
 						SIR_MAC_AUTH_FRAME_1;
