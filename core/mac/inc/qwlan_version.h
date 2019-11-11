11bc726ee73e59afa3d46367004dcfe6af95edfc
nshrivas <nshrivas@codeaurora.org>
2019-11-11 01:19:42 -0800

    Release 5.2.03.15T
    Release 5.2.03.15T
    
    Change-Id: I44a8db295e28228abf4dd16d19fd4cc75d171a57
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            03
-#define QWLAN_VERSION_EXTRA            "S"
+#define QWLAN_VERSION_EXTRA            "T"
 #define QWLAN_VERSION_BUILD            15
 
-#define QWLAN_VERSIONSTR               "5.2.03.15S"
+#define QWLAN_VERSIONSTR               "5.2.03.15T"
 
 #endif /* QWLAN_VERSION_H */
