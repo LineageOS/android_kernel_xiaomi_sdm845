bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: delete pe_session for monitor mode when iface down
Delete pe_session for monitor mode when do interface down operation.

Change-Id: I18f58906a3cff3af68558a37c2f4b3f67a8cdd22
CRs-Fixed: 2471736

core/hdd/src/wlan_hdd_main.c
@@ -5596,6 +5596,7 @@ QDF_STATUS hdd_stop_adapter(struct hdd_context *hdd_ctx,
 		wlan_hdd_scan_abort(adapter);
 		hdd_deregister_hl_netdev_fc_timer(adapter);
 		hdd_deregister_tx_flow_control(adapter);
+		sme_delete_mon_session(mac_handle, adapter->vdev_id);
 		hdd_vdev_destroy(adapter);
 		break;
 

LineageOS/android_kernel_xiaomi_sdm845

core/hdd/src/wlan_hdd_main.c
@@ -5842,6 +5842,7 @@ QDF_STATUS hdd_stop_adapter_ext(struct hdd_context *hdd_ctx,
 	case QDF_MONITOR_MODE:
 		wlan_hdd_scan_abort(adapter);
 		hdd_deregister_tx_flow_control(adapter);
+		sme_delete_mon_session(mac_handle, adapter->session_id);
 		hdd_vdev_destroy(adapter);
 		break;
 