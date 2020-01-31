3f965dd6b63d5af7bb07433030dfd775b21a81a7
1nfrag <sil3ntcll@gmail.com>
2020-01-31 16:17:38 +0800

    Techpack: Tas2557/9 Xiaomi updates for Q
    Change-Id: I7e6adf6e39cc0e7735a2493777b8aca654a2b06c

--- techpack/audio/asoc/codecs/tas2557/tas2557-codec.h
+++ techpack/audio/asoc/codecs/tas2557/tas2557-codec.h
@@ -1,6 +1,7 @@
 /*
 ** =============================================================================
 ** Copyright (c) 2016  Texas Instruments Inc.
+** Copyright (C) 2019 XiaoMi, Inc.
 **
 ** This program is free software; you can redistribute it and/or modify it under
 ** the terms of the GNU General Public License as published by the Free Software
