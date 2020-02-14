42269ad7ff11381240c7f7aa1f2976473c87a70f
Abhishek Singh <absingh@codeaurora.org>
2020-02-14 16:34:24 +0530

    qcacmn: Optimize logs in TX assoc req and RX assoc rsp
    Optimize logs while sending assoc req and assoc rsp handling
    frame by dumping the assoc req frame and not printing each
    params separately. Also avoid multiple dump of same frame
    
    Change-Id: Ibcf2d9aa8de292b9b9f4b4c6a53a0f694d69475c
    CRs-Fixed: 2622247

--- umac/p2p/core/src/wlan_p2p_main.c
+++ umac/p2p/core/src/wlan_p2p_main.c
@@ -1277,16 +1277,12 @@ QDF_STATUS p2p_status_connect(struct wlan_objmgr_vdev *vdev)
 	enum QDF_OPMODE mode;
 
 	p2p_soc_obj = get_p2p_soc_obj_by_vdev(vdev);
-	if (!p2p_soc_obj) {
-		p2p_err("P2P soc context is NULL");
+	if (!p2p_soc_obj)
 		return QDF_STATUS_E_FAILURE;
-	}
 
 	mode = wlan_vdev_mlme_get_opmode(vdev);
-	if (mode != QDF_P2P_CLIENT_MODE) {
-		p2p_debug("this is not P2P CLIENT, mode:%d", mode);
+	if (mode != QDF_P2P_CLIENT_MODE)
 		return QDF_STATUS_SUCCESS;
-	}
 
 	p2p_debug("connection status:%d", p2p_soc_obj->connection_status);
 	switch (p2p_soc_obj->connection_status) {
