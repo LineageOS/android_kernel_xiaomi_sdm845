7e0f91283870ecb097c1c72f0a1a812b53570c71
Srinivas Dasari <dasaris@codeaurora.org>
2020-02-18 23:16:38 +0530

    qcacld-3.0: Send proper reason code for STA kickout disconnections
    Currently, driver send a generic reason code
    eSIR_MAC_DISASSOC_DUE_TO_INACTIVITY_REASON to userspace when a
    STA kickout happens. Send eSIR_MAC_SA_QUERY_TIMEOUT when STA
    kickout happens due to SA query timeout.
    Send eSIR_MAC_PEER_XRETRY_FAIL when STA kickout happens due to
    XRETRY failure.
    Still host sends deauth frame with reason code
    eSIR_MAC_DISASSOC_DUE_TO_INACTIVITY_REASON.
    
    Change-Id: I72fa7406ef1b131ef3519bb6bef21cc52bc548cb
    CRs-Fixed: 2624948

--- core/mac/src/pe/lim/lim_link_monitoring_algo.c
+++ core/mac/src/pe/lim/lim_link_monitoring_algo.c
@@ -199,6 +199,7 @@ void lim_delete_sta_context(tpAniSirGlobal mac_ctx,
 	tpDeleteStaContext msg = (tpDeleteStaContext) lim_msg->bodyptr;
 	tpPESession session_entry;
 	tpDphHashNode sta_ds;
+	enum eSirMacReasonCodes reason_code;
 
 	if (NULL == msg) {
 		pe_err("Invalid body pointer in message");
@@ -213,6 +214,8 @@ void lim_delete_sta_context(tpAniSirGlobal mac_ctx,
 
 	switch (msg->reasonCode) {
 	case HAL_DEL_STA_REASON_CODE_KEEP_ALIVE:
+	case HAL_DEL_STA_REASON_CODE_SA_QUERY_TIMEOUT:
+	case HAL_DEL_STA_REASON_CODE_XRETRY:
 		if (LIM_IS_STA_ROLE(session_entry) && !msg->is_tdls) {
 			if (!((session_entry->limMlmState ==
 			    eLIM_MLM_LINK_ESTABLISHED_STATE) &&
@@ -239,10 +242,18 @@ void lim_delete_sta_context(tpAniSirGlobal mac_ctx,
 			lim_send_deauth_mgmt_frame(mac_ctx,
 				eSIR_MAC_DISASSOC_DUE_TO_INACTIVITY_REASON,
 				msg->addr2, session_entry, false);
+			if (msg->reasonCode ==
+				HAL_DEL_STA_REASON_CODE_SA_QUERY_TIMEOUT)
+				reason_code = eSIR_MAC_SA_QUERY_TIMEOUT;
+			else if (msg->reasonCode ==
+				HAL_DEL_STA_REASON_CODE_XRETRY)
+				reason_code = eSIR_MAC_PEER_XRETRY_FAIL;
+			else
+				reason_code = eSIR_MAC_PEER_INACTIVITY;
 			lim_tear_down_link_with_ap(mac_ctx,
-				session_entry->peSessionId,
-				eSIR_MAC_DISASSOC_DUE_TO_INACTIVITY_REASON,
-				eLIM_LINK_MONITORING_DEAUTH);
+						   session_entry->peSessionId,
+						   reason_code,
+						   eLIM_LINK_MONITORING_DEAUTH);
 			/* only break for STA role (non TDLS) */
 			break;
 		}
@@ -267,7 +278,7 @@ void lim_delete_sta_context(tpAniSirGlobal mac_ctx,
 			return;
 		}
 		lim_send_deauth_mgmt_frame(mac_ctx,
-				eSIR_MAC_DISASSOC_DUE_TO_INACTIVITY_REASON,
+				eSIR_MAC_BSS_TRANSITION_DISASSOC,
 				session_entry->bssId, session_entry, false);
 		lim_tear_down_link_with_ap(mac_ctx, session_entry->peSessionId,
 					   eSIR_MAC_BSS_TRANSITION_DISASSOC,
