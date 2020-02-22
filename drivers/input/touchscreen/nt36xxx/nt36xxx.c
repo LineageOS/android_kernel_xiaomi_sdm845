48dfde7629a8c5a6574eca233460df095bf516d4
Pierre2324 <pdbbasketball@gmail.com>
2020-02-22 11:11:55 -0500

    touchscreen: nt36xxx: Fix DT2W procfs node
    OnePlus Ports use a different node name, fix that.
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>
    Signed-off-by: sajidshahriar72543 <sazidshahriar39@gmail.com>

--- drivers/input/touchscreen/nt36xxx/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -1541,14 +1541,14 @@ static ssize_t nvt_panel_display_show(struct device *dev,
 	return snprintf(buf, PAGE_SIZE, "%c\n", ts->lockdown_info[1]);
 }
 
-static ssize_t nvt_panel_wake_gesture_show(struct device *dev,
+static ssize_t nvt_panel_gesture_enable_show(struct device *dev,
 				     struct device_attribute *attr, char *buf)
 {
         const char c = ts->gesture_enabled ? '1' : '0';
         return sprintf(buf, "%c\n", c);
 }
 
-static ssize_t nvt_panel_wake_gesture_store(struct device *dev,
+static ssize_t nvt_panel_gesture_enable_store(struct device *dev,
 				     struct device_attribute *attr, const char *buf, size_t count)
 {
 	int i;
@@ -1557,7 +1557,7 @@ static ssize_t nvt_panel_wake_gesture_store(struct device *dev,
 		ts->gesture_enabled = i;
 		return count;
 	} else {
-		dev_dbg(dev, "enable_dt2w write error\n");
+		dev_dbg(dev, "gesture_enable write error\n");
 		return -EINVAL;
 	}
 }
@@ -1565,14 +1565,14 @@ static ssize_t nvt_panel_wake_gesture_store(struct device *dev,
 static DEVICE_ATTR(panel_vendor, (S_IRUGO), nvt_panel_vendor_show, NULL);
 static DEVICE_ATTR(panel_color, (S_IRUGO), nvt_panel_color_show, NULL);
 static DEVICE_ATTR(panel_display, (S_IRUGO), nvt_panel_display_show, NULL);
-static DEVICE_ATTR(wake_gesture, S_IWUSR | S_IRUSR,
-		nvt_panel_wake_gesture_show, nvt_panel_wake_gesture_store);
+static DEVICE_ATTR(gesture_enable, S_IWUSR | S_IRUSR,
+		nvt_panel_gesture_enable_show, nvt_panel_gesture_enable_store);
 
 static struct attribute *nvt_attr_group[] = {
 	&dev_attr_panel_vendor.attr,
 	&dev_attr_panel_color.attr,
 	&dev_attr_panel_display.attr,
-	&dev_attr_wake_gesture.attr,
+	&dev_attr_gesture_enable.attr,
 	NULL,
 };
 
