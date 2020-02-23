3eccd76b81f96922eedacd69a336c58432123668
Vulupala Shashank Reddy <vulupa@codeaurora.org>
2020-02-23 13:04:51 +0530

    qcacmn: Add new radiotap header fields
    Add channel noise floor for the pdev.
    
    Change-Id: I6c30f0f23eb426977424c30ca3e7308f221c5ab3
    CRs-Fixed: 2627701

--- qdf/inc/qdf_nbuf.h
+++ qdf/inc/qdf_nbuf.h
@@ -208,6 +208,7 @@
  * @he_data5: HE property of received frame
  * @prev_ppdu_id: ppdu_id in previously received message
  * @ppdu_id: Id of the PLCP protocol data unit
+ * @chan_noise_floor: Channel Noise Floor for the pdev
  */
 struct mon_rx_status {
 	uint64_t tsft;
@@ -274,6 +275,7 @@ struct mon_rx_status {
 	uint32_t ppdu_len;
 	uint32_t prev_ppdu_id;
 	uint32_t ppdu_id;
+	int16_t chan_noise_floor;
 };
 
 /* Masks for HE SIG known fields in mon_rx_status structure */
