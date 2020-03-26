01fcaa88cabf99b68622d3c4806b43f10276d511
cluelessfirefly <61371093+cluelessfirefly@users.noreply.github.com>
2020-03-27 00:57:57 +0700

    Update eq.sh
    Update build script.
    I use these 3, Clang 10.0.5, aarch64-linux-android-4.9, arm-linux-androideabi-4.9 , as my toolchain.

--- eq.sh
+++ eq.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
 KERNEL_DIR=$(pwd)
-ANYKERNEL_DIR=$KERNEL_DIR/anykernel
+ANYKERNEL_DIR=$KERNEL_DIR/anykernel/equuleus
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="clueless"
 DEVICE="-equuleus-"
-VER="-v0.1"
+VER="v01"
 TYPE="-AOSP"
 FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
 
-rm -rf $ANYKERNEL_DIR/Image.gz-dtb
-rm -rf $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+rm -rf $ANYKERNEL_DIR//equuleus/Image.gz-dtb
+rm -rf $KERNEL_DIR/out/arch/arm64/boot/Image.gz $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 rm -rf $FINAL_ZIP
-rm -rf out/
 
-make clean && make mrproper
+mkdir -p out
+make O=out clean
+make O=out mrproper
 make O=out ARCH=arm64 equuleus_defconfig
 
 
-PATH="/home/clueless/android/toolchain/linux-x86/clang-r377782c/bin:/home/clueless/android/toolchain/aarch64-linux-android-4.9/bin:${PATH}" \
+PATH="~/android/toolchain/linux-x86/clang-r377782c/bin:~/android/toolchain/aarch64-linux-android-4.9/bin:~/android/toolchain/arm-linux-androideabi-4.9/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC=clang \
+                      CXX=clang++ \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE=aarch64-linux-android- 
-                      
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+                      CCHACHE=1 \
+                      CCACHE_COMPRESS=1
+
+
+
 {
-cp $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR;
+cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/equuleus;
 }
 
 
 
-cd $ANYKERNEL_DIR
+cd $ANYKERNEL_DIR/equuleus
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
 
 cd
 cd $KERNEL_DIR
-
 
