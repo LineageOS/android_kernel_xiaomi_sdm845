dee267688a97502bcda7a032be6ea2ea9b571e10
nshrivas <nshrivas@codeaurora.org>
2020-03-27 01:18:41 -0700

    Release 5.2.03.24X
    Release 5.2.03.24X
    
    Change-Id: Id0f515a536cced96e6cdf1b7addd54cd146c960c
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            03
-#define QWLAN_VERSION_EXTRA            "W"
+#define QWLAN_VERSION_EXTRA            "X"
 #define QWLAN_VERSION_BUILD            24
 
-#define QWLAN_VERSIONSTR               "5.2.03.24W"
+#define QWLAN_VERSIONSTR               "5.2.03.24X"
 
 #endif /* QWLAN_VERSION_H */
