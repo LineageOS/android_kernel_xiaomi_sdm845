46cff984eef38e43b6abe6550028f554a85752c9
Sultan Alsawaf <sultan@kerneltoast.com>
2020-05-01 16:38:46 -0700

    cpuidle: Optimize pm_qos notifier callback and IPI semantics
    The pm_qos callback currently suffers from a number of pitfalls: it
    sends IPIs to CPUs that may not be idle, waits for those IPIs to finish
    propagating while preemption is disabled (resulting in a long busy wait
    for the pm_qos_update_target() caller), and needlessly calls a no-op
    function when the IPIs are processed.
    
    Optimize the pm_qos notifier by only sending IPIs to CPUs that are
    idle, and by using arch_send_wakeup_ipi_mask() instead of
    smp_call_function_many(). Using IPI_WAKEUP instead of IPI_CALL_FUNC,
    which is what smp_call_function_many() uses behind the scenes, has the
    benefit of doing zero work upon receipt of the IPI; IPI_WAKEUP is
    designed purely for sending an IPI without a payload, whereas
    IPI_CALL_FUNC does unwanted extra work just to run the empty
    smp_callback() function.
    
    Determining which CPUs are idle is done efficiently with an atomic
    bitmask instead of using the wake_up_if_idle() API, which checks the
    CPU's runqueue in an RCU read-side critical section and under a spin
    lock. Not very efficient in comparison to a simple, atomic bitwise
    operation. A cpumask isn't needed for this because NR_CPUS is
    guaranteed to fit within a word.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [yaro: moved the meat wake_up_idle_cpus to reduce the size of a diff]
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -35,6 +35,27 @@ static int enabled_devices;
 static int off __read_mostly;
 static int initialized __read_mostly;
 
+#ifdef CONFIG_SMP
+static atomic_t idled = ATOMIC_INIT(0);
+
+#if NR_CPUS > 32
+#error idled CPU mask not big enough for NR_CPUS
+#endif
+
+static void cpuidle_set_idle_cpu(unsigned int cpu)
+{
+	atomic_or(BIT(cpu), &idled);
+}
+
+static void cpuidle_clear_idle_cpu(unsigned int cpu)
+{
+	atomic_andnot(BIT(cpu), &idled);
+}
+#else
+static inline void cpuidle_set_idle_cpu(unsigned int cpu) { }
+static inline void cpuidle_clear_idle_cpu(unsigned int cpu) { }
+#endif
+
 int cpuidle_disabled(void)
 {
 	return off;
@@ -199,7 +220,9 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	time_start = ns_to_ktime(local_clock());
 
 	stop_critical_timings();
+	cpuidle_set_idle_cpu(dev->cpu);
 	entered_state = target_state->enter(dev, drv, index);
+	cpuidle_clear_idle_cpu(dev->cpu);
 	start_critical_timings();
 
 	time_end = ns_to_ktime(local_clock());
@@ -616,22 +639,12 @@ EXPORT_SYMBOL_GPL(cpuidle_register);
 
 static void wake_up_idle_cpus(void *v)
 {
-	int cpu;
-	struct cpumask cpus;
+	unsigned long cpus = atomic_read(&idled) & *cpumask_bits(to_cpumask(v));
 
-	preempt_disable();
-	if (v) {
-		cpumask_andnot(&cpus, v, cpu_isolated_mask);
-		cpumask_and(&cpus, &cpus, cpu_online_mask);
-	} else
-		cpumask_andnot(&cpus, cpu_online_mask, cpu_isolated_mask);
-
-	for_each_cpu(cpu, &cpus) {
-		if (cpu == smp_processor_id())
-			continue;
-		wake_up_if_idle(cpu);
-	}
-	preempt_enable();
+	/* Use READ_ONCE to get the isolated mask outside cpu_add_remove_lock */
+	cpus &= ~READ_ONCE(*cpumask_bits(cpu_isolated_mask));
+	if (cpus)
+		arch_send_wakeup_ipi_mask(to_cpumask(&cpus));
 }
 
 /*
