dea22eb4972c251391f6e08538719a3d5d0d7805
Khusika Dhamar Gusti <mail@khusika.com>
2020-05-30 21:30:40 +0700

    treewide: Fix CAF mismerge in db193f5
    * arch/arm64/kernel/smp.c:
        num_other_active_cpus has the same function with num_other_online_cpus,
        remove one of them to avoid redundant code.
    
    * arch/arm64/mm/mmu.c:
        Remove redundant pmd_t and pud_t introduced in db193f5, also
        fix CAF mismerge (76d3252) with this commit 120db14
    
    * drivers/scsi/sd.c:
        CAF omitted commit 95b526e from android-4.9.218 (d2440d6)
    
    Change-Id: I6137bade98d3eabf62c95d433f3fd6bec8471ae9
    Signed-off-by: Khusika Dhamar Gusti <mail@khusika.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -28,8 +28,6 @@
 #include <linux/memblock.h>
 #include <linux/fs.h>
 #include <linux/io.h>
-#include <linux/slab.h>
-#include <linux/stop_machine.h>
 #include <linux/dma-contiguous.h>
 #include <linux/cma.h>
 #include <linux/mm.h>
@@ -214,7 +212,6 @@ static void alloc_init_pmd(pud_t *pud, unsigned long addr, unsigned long end,
 		if (((addr | next | phys) & ~SECTION_MASK) == 0 &&
 		      allow_block_mappings &&
 		      !dma_overlap(phys, phys + next - addr)) {
-			pmd_t old_pmd =*pmd;
 			pmd_set_huge(pmd, phys, prot);
 
 			/*
@@ -275,7 +272,6 @@ static void alloc_init_pud(pgd_t *pgd, unsigned long addr, unsigned long end,
 		 */
 		if (use_1G_block(addr, next, phys) && allow_block_mappings &&
 		    !dma_overlap(phys, phys + next - addr)) {
-			pud_t old_pud = *pud;
 			pud_set_huge(pud, phys, prot);
 
 			/*
