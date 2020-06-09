ae57cb5ac4bdcdeb179c126a621e637b01e846ee
Srikanth Marepalli <marepall@codeaurora.org>
2020-06-09 22:49:00 +0530

    qcacld-3.0: Process external auth command and set preauth node state
    Hostapd sends SAE authentication status with the NL command
    NL80211_CMD_EXTERNAL_AUTH. Extract status and peer mac address
    from the command data and set mlmState in preauth node accordingly.
    
    Change-Id: If507a2f56c031ae1885a11d5f7cbe31a18aa8821
    CRs-Fixed: 2396366

--- core/sme/src/common/sme_api.c
+++ core/sme/src/common/sme_api.c
@@ -16645,8 +16645,13 @@ QDF_STATUS sme_handle_sae_msg(tHalHandle hal, uint8_t session_id,
 		sae_msg->length = sizeof(*sae_msg);
 		sae_msg->session_id = session_id;
 		sae_msg->sae_status = sae_status;
-		sme_debug("SAE: sae_status %d session_id %d",
-			  sae_msg->sae_status, sae_msg->session_id);
+		qdf_mem_copy(sae_msg->peer_mac_addr,
+			     peer_mac_addr.bytes,
+			     QDF_MAC_ADDR_SIZE);
+		sme_debug("SAE: sae_status %d session_id %d Peer: "
+			  MAC_ADDRESS_STR, sae_msg->sae_status,
+			  sae_msg->session_id,
+			  MAC_ADDR_ARRAY(sae_msg->peer_mac_addr));
 
 		sch_msg.type = eWNI_SME_SEND_SAE_MSG;
 		sch_msg.bodyptr = sae_msg;
