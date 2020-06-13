ff3fe7fa56975482d00dbe60016c82a7d6be8d06
Tyler Nijmeh <tylernij@gmail.com>
2020-06-12 19:32:18 -0700

    tcp: fastopen: Enable cookieless TFO clients and servers
    Change-Id: Iddca67a2ac3cd0bbeb22174ffd45f17665eba4fc
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: awkoo <awkooox@gmail.com>

--- net/ipv4/tcp_fastopen.c
+++ net/ipv4/tcp_fastopen.c
@@ -9,7 +9,11 @@
 #include <net/inetpeer.h>
 #include <net/tcp.h>
 
-int sysctl_tcp_fastopen __read_mostly = TFO_CLIENT_ENABLE;
+int sysctl_tcp_fastopen __read_mostly = TFO_CLIENT_ENABLE |
+					TFO_SERVER_ENABLE |
+					TFO_CLIENT_NO_COOKIE |
+					TFO_SERVER_COOKIE_NOT_REQD |
+					TFO_SERVER_WO_SOCKOPT1;
 
 struct tcp_fastopen_context __rcu *tcp_fastopen_ctx;
 
