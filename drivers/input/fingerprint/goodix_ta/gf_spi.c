30e030b8337a50c13f482b69883b1766021aa79a
Demon000 <demonsingur@gmail.com>
2020-07-05 12:34:56 +0530

    fingerprint: goodix_ta: use qcom drm notifier
    Change-Id: Ia1902c94ba4c35bfb69319ebf0a5a1eb2b8f2041
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>
    Signed-off-by: sajidshahriar72543 <sazidshahriar39@gmail.com>

--- drivers/input/fingerprint/goodix_ta/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta/gf_spi.c
@@ -43,7 +43,7 @@
 #include <linux/cpufreq.h>
 #include <linux/pm_wakeup.h>
 #include <drm/drm_bridge.h>
-#include <drm/drm_notifier.h>
+#include <linux/msm_drm_notify.h>
 
 #include "gf_spi.h"
 
@@ -728,7 +728,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 	if (evdata && evdata->data && val == DRM_EVENT_BLANK && gf_dev) {
 		blank = *(int *)(evdata->data);
 		switch (blank) {
-		case DRM_BLANK_POWERDOWN:
+		case MSM_DRM_BLANK_POWERDOWN:
 			if (gf_dev->device_available == 1) {
 				gf_dev->fb_black = 1;
 				gf_dev->wait_finger_down = true;
@@ -742,7 +742,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 #endif
 			}
 			break;
-		case DRM_BLANK_UNBLANK:
+		case MSM_DRM_BLANK_UNBLANK:
 			if (gf_dev->device_available == 1) {
 				gf_dev->fb_black = 0;
 #if defined(GF_NETLINK_ENABLE)
@@ -856,7 +856,7 @@ static int gf_probe(struct platform_device *pdev)
 #endif
 
 	gf_dev->notifier = goodix_noti_block;
-	drm_register_client(&gf_dev->notifier);
+	msm_drm_register_client(&gf_dev->notifier);
 
 	gf_dev->irq = gf_irq_num(gf_dev);
 
@@ -924,7 +924,7 @@ static int gf_remove(struct platform_device *pdev)
 		gf_cleanup(gf_dev);
 
 
-	drm_unregister_client(&gf_dev->notifier);
+	msm_drm_unregister_client(&gf_dev->notifier);
 	mutex_unlock(&device_list_lock);
 
 	return 0;
