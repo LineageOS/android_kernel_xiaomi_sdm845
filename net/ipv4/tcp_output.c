bc5cfa8ff1785686fa2fb4ac09a64903052e3a9f
Tyler Nijmeh <tylernij@gmail.com>
2020-07-11 12:32:22 -0700

    tcp: Disable slow start after idle
    Change-Id: Ic0bd4207b8b318d499f0e833ca0374231caef4dd
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: awkoo <awkooox@gmail.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -60,7 +60,7 @@ int sysctl_tcp_limit_output_bytes __read_mostly = 262144;
 int sysctl_tcp_tso_win_divisor __read_mostly = 3;
 
 /* By default, RFC2861 behavior.  */
-int sysctl_tcp_slow_start_after_idle __read_mostly = 1;
+int sysctl_tcp_slow_start_after_idle __read_mostly;
 
 static bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 			   int push_one, gfp_t gfp);
