75d23cd208fb44679a7e26e3f20c4ca3a7572a1b
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2020-07-20 11:56:31 +0530

    qcacld-3.0: Complete the wait events out of the lock in ssr
    Currently When SSR occurs, wait events gets complete in
    init_deinit lock, there is a case when ssr within ssr occurs
    the second ssr waits for init deinit mutex lock which is held
    by the first ssr in the process of reinit, in the reinit there
    are some events for which there are waits one such event is in
    htc_wait_recv_ctrl_message api which waits for BMI
    request/response to complete and because of which the second ssr
    also waits for the mutex which leads to issues in the modem
    subsystem.
    
    To address above issue, complete the wait events out of the
    mutex so that when second ssr comes it completes all the wait
    events before it starts wait on the mutex so that first ssr
    releases the mutex as it does not wait on any event.
    
    Change-Id: Ifdf75a057a585eb4ba744c1145c146f7c6927691
    CRs-Fixed: 2735824

--- core/hdd/src/wlan_hdd_driver_ops.c
+++ core/hdd/src/wlan_hdd_driver_ops.c
@@ -1519,12 +1519,10 @@ static void wlan_hdd_set_the_pld_uevent(struct pld_uevent_data *uevent)
 {
 	switch (uevent->uevent) {
 	case PLD_RECOVERY:
-		cds_set_target_ready(false);
-		cds_set_recovery_in_progress(true);
-		break;
 	case PLD_FW_DOWN:
 		cds_set_target_ready(false);
 		cds_set_recovery_in_progress(true);
+		qdf_complete_wait_events();
 		break;
 	case PLD_FW_HANG_EVENT:
 		break;
@@ -1565,17 +1563,13 @@ static void wlan_hdd_handle_the_pld_uevent(struct pld_uevent_data *uevent)
 
 	switch (uevent->uevent) {
 	case PLD_RECOVERY:
-		cds_set_target_ready(false);
 		hdd_pld_ipa_uc_shutdown_pipes();
-		qdf_complete_wait_events();
 		break;
 	case PLD_FW_DOWN:
-		cds_set_target_ready(false);
 		wlan_cfg80211_cleanup_scan_queue(hdd_ctx->pdev, NULL);
 		if (pld_is_fw_rejuvenate(hdd_ctx->parent_dev) &&
 		    ucfg_ipa_is_enabled())
 			ucfg_ipa_fw_rejuvenate_send_msg(hdd_ctx->pdev);
-		qdf_complete_wait_events();
 		break;
 	case PLD_FW_HANG_EVENT:
 		hdd_info("Received fimrware hang event");
@@ -1633,6 +1627,7 @@ static void wlan_hdd_pld_uevent(struct device *dev,
 	wlan_hdd_set_the_pld_uevent(uevent);
 
 	hdd_psoc_idle_timer_stop(hdd_ctx);
+
 	mutex_lock(&hdd_init_deinit_lock);
 	wlan_hdd_handle_the_pld_uevent(uevent);
 	mutex_unlock(&hdd_init_deinit_lock);
