c1fcbfa9a7acf477795d7f7c992c42a4f171a789
Kailash <kailash.sudhakar@gmail.com>
2020-08-19 01:34:13 +0530

    drivers: adsprpc: Replace cpumask usage with raw bitwise op
    Signed-off-by: Kailash <kailash.sudhakar@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -3720,7 +3720,7 @@ static int fastrpc_internal_control(struct fastrpc_file *fl,
 		if (err)
 			goto bail;
 		fl->pm_qos_req.type = PM_QOS_REQ_AFFINE_CORES;
-		cpumask_copy(&fl->pm_qos_req.cpus_affine, cpu_lp_mask);
+		atomic_set(&fl->pm_qos_req.cpus_affine, *cpumask_bits(cpu_lp_mask));
 		if (!fl->qos_request) {
 			pm_qos_add_request(&fl->pm_qos_req,
 				PM_QOS_CPU_DMA_LATENCY, latency);
