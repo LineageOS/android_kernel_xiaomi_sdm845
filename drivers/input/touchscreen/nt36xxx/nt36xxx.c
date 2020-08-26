a2e5dc2f7881b2a62da8ba8e2a33710d5a358f99
Rapherion Rollerscaperers <rapherion@raphielgang.org>
2020-08-26 19:09:45 +0700

    input / touchscreen: nt36xxx: Require low latency
    Currently, CPU can enter deep sleep while waiting for data on the i2c
    bug. That means an i2c read of 8 bytes can take as long as 10 ms, which
    would overrun the desired interrupt handling time.
    Use pm qos to require low latency and prevent CPU from entering deep
    sleep while handling touch interrupts.
    
    Also optimize it by only waking up a core that's servicing and IRQ.
    
    Ref https://github.com/aosp-mirror/kernel_msm/commit/c775f94bab51bfd1820fa0ff4be9a584ee6d34b0
    
    Change-Id: I22a70b74cc59a665496de2d429253a8421621299
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

--- drivers/input/touchscreen/nt36xxx/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -1207,6 +1207,8 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 	int32_t i = 0;
 	int32_t finger_cnt = 0;
 
+	pm_qos_update_request(&ts->pm_qos_req, 100);
+
 #if WAKEUP_GESTURE
 	if (likely(bTouchIsAwake == 0)) {
 		pm_wakeup_event(&ts->input_dev->dev, 5000);
@@ -1233,8 +1235,7 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 		input_id = (uint8_t)(point_data[1] >> 3);
 		nvt_ts_wakeup_gesture_report(input_id, point_data);
 		nvt_irq_enable(true);
-		mutex_unlock(&ts->lock);
-		return IRQ_HANDLED;
+		goto XFER_ERROR;
 	}
 #endif
 
@@ -1327,9 +1328,11 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 	}
 #endif
 
-	input_sync(ts->input_dev);
-
 XFER_ERROR:
+
+	pm_qos_update_request(&ts->pm_qos_req, PM_QOS_DEFAULT_VALUE);
+
+	input_sync(ts->input_dev);
 
 	mutex_unlock(&ts->lock);
 
@@ -1687,6 +1690,11 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 			nvt_irq_enable(false);
 			NVT_LOG("request irq %d succeed\n", client->irq);
 		}
+
+		ts->pm_qos_req.type = PM_QOS_REQ_AFFINE_IRQ;
+		ts->pm_qos_req.irq = ts->client->irq;
+		pm_qos_add_request(&ts->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
+				PM_QOS_DEFAULT_VALUE);
 	}
 	ts->fw_name = nvt_get_config(ts);
 
@@ -1835,6 +1843,8 @@ static int32_t nvt_ts_remove(struct i2c_client *client)
 	if (drm_unregister_client(&ts->notifier))
 		NVT_ERR("Error occurred while unregistering drm_notifier.\n");
 #endif
+
+	pm_qos_remove_request(&ts->pm_qos_req);
 
 #if NVT_TOUCH_MP
 	nvt_mp_proc_deinit();
