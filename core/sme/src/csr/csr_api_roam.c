e564cfbe69d4c8fe66c0808ab3d1344cda334bad
Srikanth Marepalli <marepall@codeaurora.org>
2020-10-04 18:45:14 +0530

    qcacld-3.0: Fix to avoid sending invalid PMKID in FT-SuiteB roam
    In FT-SuiteB when STA connects to AP1 then PMK1 gets cached. And
    then STA disconnects from AP1 and connects to AP2 then PMK2
    gets cached. Now if STA roams to AP1 then FW uses PMK2 from mlme
    session to create the PMKID. But the corresponding RSO command
    from driver sends the PMK1 to FW and the same gets updated in
    mlme session of FW. This results in failure of next roams, as the
    invalid PMKID gets created using PMK1.
    This fix helps in deleting the old/stale PMK cache entries that
    are under the same mobility domain as of the newly added entry in
    case of all the FT connections except FT-SAE.
    
    Change-Id: Id960e955f8669e19e950c1edc38a9f0a715292b7
    CRs-Fixed: 2798713

--- core/sme/src/csr/csr_api_roam.c
+++ core/sme/src/csr/csr_api_roam.c
@@ -16151,6 +16151,7 @@ csr_roam_set_pmkid_cache(tpAniSirGlobal pMac, uint32_t sessionId,
 	struct csr_roam_session *pSession = CSR_GET_SESSION(pMac, sessionId);
 	uint32_t i = 0;
 	tPmkidCacheInfo *pmksa;
+	eCsrAuthType akm_type;
 
 	if (!pSession) {
 		sme_err("session %d not found", sessionId);
@@ -16185,6 +16186,17 @@ csr_roam_set_pmkid_cache(tpAniSirGlobal pMac, uint32_t sessionId,
 		/* Update new entry */
 		csr_update_pmk_cache(pSession, pmksa);
 
+		akm_type = pSession->connectedProfile.AuthType;
+		if ((akm_type == eCSR_AUTH_TYPE_FT_RSN ||
+		     akm_type == eCSR_AUTH_TYPE_FT_FILS_SHA256 ||
+		     akm_type == eCSR_AUTH_TYPE_FT_FILS_SHA384 ||
+		     akm_type == eCSR_AUTH_TYPE_FT_SUITEB_EAP_SHA384) &&
+		    pSession->connectedProfile.MDID.mdiePresent) {
+			sme_debug("Auth type is %d update the MDID in cache",
+				  akm_type);
+			csr_update_pmk_cache_ft(pMac,
+						sessionId, pmksa->cache_id);
+		}
 	}
 	return QDF_STATUS_SUCCESS;
 }
