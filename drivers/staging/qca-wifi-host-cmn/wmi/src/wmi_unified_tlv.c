264f10085e09f4f9484ecf440adca292925b8d31
Chenyang Zhong <zhongcy95@gmail.com>
2020-12-11 19:24:32 -0500

    qcacmn: wmi: silence log spam on out-of-range service_id
    dmesg:
    wlan: [2130:E:WMI] Service id 258 but WMI ext2 service bitmap is NULL
    ...
    
    Even before commit "qcacmn: Add support for wmi_ext2 service
    bitmap", macro WMI_SERVICE_EXT_IS_ENABLED would perform the same
    comparison and return false on out-of-range service_id. Therefore,
    silence the error spam to have a peace of mind.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Ia70b11bfaaa746a58a3cd2582f4158b34562fc5b

--- drivers/staging/qca-wifi-host-cmn/wmi/src/wmi_unified_tlv.c
+++ drivers/staging/qca-wifi-host-cmn/wmi/src/wmi_unified_tlv.c
@@ -18150,7 +18150,7 @@ static bool is_service_enabled_tlv(wmi_unified_t wmi_handle,
 	}
 
 	if (service_id >= WMI_MAX_EXT_SERVICE) {
-		WMI_LOGE("Service id %d but WMI ext2 service bitmap is NULL",
+		WMI_LOGD("Service id %d but WMI ext2 service bitmap is NULL",
 			 service_id);
 		return false;
 	}
