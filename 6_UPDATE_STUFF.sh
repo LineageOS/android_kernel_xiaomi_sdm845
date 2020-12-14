ebc87593795082ecc86b340a5fd8a5e2a4d50d64
thanasxda <thanasxda 15927885+thanasxda@users.noreply.github.com>
2020-12-14 23:55:44 +0200

    sh

--- 6_UPDATE_STUFF.sh
+++ 6_UPDATE_STUFF.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt -f install -y aptitude
 sudo aptitude -f install -y  libomp-12-dev llvm-12 llvm llvm-12 clang-12 lld-12 gcc clang binutils make flex bison bc build-essential libncurses-dev libssl-dev libelf-dev qt5-default libclang-common-12-dev gcc-arm-linux-gnueabi gcc-aarch64-linux-gnu
 sudo apt -f --fix-missing install -y
-sudo aptitude -f full-upgrade -y
+sudo aptitude -f upgrade -y --with-new-pkgs
 cd $MLX && git pull
 cd $AK && git pull
 cd $AIK && git pull
