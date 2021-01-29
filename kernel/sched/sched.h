da07165707b54d27b87fc703e43e9313dc5269ed
darkhz <kmachanwenw@gmail.com>
2021-01-29 12:02:10 +0530

    sched/uclamp: Make uclamp_boosted() return proper boosted value
    The previous implementation of uclamp_boosted took the effective
    UCLAMP_MIN of a task and checked if it is greater than 0, and if so,
    then said task would be boosted.
    
    The UCLAMP_MIN value for all tasks returned the systemwide
    minimum util clamp value (i.e, kernel.sched_util_clamp_min), and
    if set to a non-zero value, will result in all tasks being boosted,
    which is undesirable.
    
    Hence, we return the proper boosted value set in a taskgroup.To set
    this boosted value for a taskgroup in userspace, however, we need another
    tunable.This tunable will be introduced in the next patch.
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -388,6 +388,8 @@ struct task_group {
 	struct uclamp_se	uclamp[UCLAMP_CNT];
 	/* Latency-sensitive flag used for a task group */
 	unsigned int		latency_sensitive;
+	/* Boosted flag for a task group */
+	unsigned int 		boosted;
 #endif
 
 };
@@ -2531,11 +2533,6 @@ static inline bool uclamp_is_used(void)
 {
 	return static_branch_likely(&sched_uclamp_used);
 }
-
-static inline bool uclamp_boosted(struct task_struct *p)
-{
-	return uclamp_eff_value(p, UCLAMP_MIN) > 0;
-}
 #else /* CONFIG_UCLAMP_TASK */
 static inline unsigned long uclamp_util_with(struct rq *rq, unsigned long util,
 					     struct task_struct *p)
@@ -2565,8 +2562,29 @@ static inline bool uclamp_latency_sensitive(struct task_struct *p)
 
 	return tg->latency_sensitive;
 }
+
+static inline bool uclamp_boosted(struct task_struct *p)
+{
+	struct cgroup_subsys_state *css = task_css(p, cpuset_cgrp_id);
+	struct task_group *tg;
+
+	if (!css)
+		return false;
+
+	if (!strlen(css->cgroup->kn->name))
+		return 0;
+
+	tg = container_of(css, struct task_group, css);
+
+	return tg->boosted;
+}
 #else
 static inline bool uclamp_latency_sensitive(struct task_struct *p)
+{
+	return false;
+}
+
+static inline bool uclamp_boosted(struct task_struct *p)
 {
 	return false;
 }
