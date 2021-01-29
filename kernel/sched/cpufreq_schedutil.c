23b819e11fb1b45d19f4dab1128f7fdf5c2aa034
darkhz <kmachanwenw@gmail.com>
2021-01-29 11:23:56 +0530

    sched/cpufreq_schedutil: Reflect uclamp changes
    This is a follow-up of:
    BACKPORT: sched/cpufreq, sched/uclamp: Add clamps for FAIR and RT tasks
    
    We excluded the schedutil-related change there since the differences
    between 4.9 and 4.19 schedutil sources were very big, and proceed to
    modify sugov_get_util() according to the above commit.
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -209,6 +209,11 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu)
 	*max = cfs_max;
 
 	*util = boosted_cpu_util(cpu, &loadcpu->walt_load);
+	
+#ifdef CONFIG_UCLAMP_TASK
+	*util = uclamp_util_with(rq, *util, NULL);
+	*util = min(*max, *util);
+#endif
 }
 
 static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
