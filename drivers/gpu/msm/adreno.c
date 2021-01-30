21922c3cbb09081dcda9ee8df7261eb1a08c1532
CloudedQuartz <ravenklawasd@gmail.com>
2021-01-30 09:09:09 +0530

    drivers: adreno: properly set idle timeout to 58ms
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1045,7 +1045,7 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 		device->pwrctrl.pm_qos_wakeup_latency = 101;
 
 	if (of_property_read_u32(node, "qcom,idle-timeout", &timeout))
-		timeout = 64;
+		timeout = 58;
 
 	device->pwrctrl.interval_timeout = msecs_to_jiffies(timeout);
 
