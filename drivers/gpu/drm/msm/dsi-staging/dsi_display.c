685a122e394d787f7c8d6ae68924e46b153821c1
Albert I <kras@raphielgang.org>
2021-03-28 00:25:14 +0800

    drm: msm: dsi-staging: Create symlink of active panel as dsi-display-primary
    In order to support mi9fod implementation, current implementation of fingerprint
    under display HAL expects path to primary display as qcom,dsi-display-primary.
    However, on msm-4.9 dsi-staging normally creates sysfs under current active
    display during boot that'll never be same for all devices.
    
    Normally, commit 10e9b127e1a8 ("drm/msm/dsi-staging: optimize dsi probe") from
    msm-4.14 needs to be backported. However, due to potential headaches will be put
    when doing so and how it'll outperform the necessity of the backport, let's just
    create a kernfs symlink of current active display as qcom,dsi-display-primary
    and leave it alone.
    
    Test: Compiled, device boots, node symlink exists
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I7a1f9e454371711e1593b41858da67e8d2b9c5d2

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -20,6 +20,7 @@
 #include <linux/of_graph.h>
 #include <linux/of_gpio.h>
 #include <linux/err.h>
+#include <linux/kernfs.h>
 #include <drm/drm_notifier.h>
 
 #include "msm_drv.h"
@@ -63,6 +64,8 @@ static const struct of_device_id dsi_display_dt_match[] = {
 
 static struct dsi_display *primary_display;
 static struct dsi_display *secondary_display;
+
+static struct kernfs_node *dsi_link;
 
 const char *dsi_get_display_name(void)
 {
@@ -5064,6 +5067,23 @@ static int dsi_display_sysfs_init(struct dsi_display *display)
 {
 	int rc = 0;
 	struct device *dev = &display->pdev->dev;
+	struct device *soc_dev = dev->parent;
+
+	if (!soc_dev)
+		pr_err("[%s] unable to determine parent device\n", display->name);
+	else {
+		struct kobject *dsi_kobj = &dev->kobj;
+		struct kernfs_node *dsi_node = dsi_kobj->sd;
+
+		kernfs_get(dsi_node);
+
+		dsi_link = kernfs_create_link(soc_dev->kobj.sd, "soc:qcom,dsi-display-primary",
+					      dsi_node);
+		if (IS_ERR_OR_NULL(dsi_link))
+			pr_err("[%s] unable to create dsi-display symlink\n", display->name);
+
+		kernfs_put(dsi_node);
+	}
 
 	rc = sysfs_create_group(&dev->kobj, &display_fs_attrs_group);
 	if (rc)
@@ -5087,6 +5107,9 @@ static int dsi_display_sysfs_deinit(struct dsi_display *display)
 			&dynamic_dsi_clock_fs_attrs_group);
 
 	sysfs_remove_group(&dev->kobj, &display_fs_attrs_group);
+
+	if (!IS_ERR_OR_NULL(dsi_link))
+		kernfs_remove_by_name(dsi_link->parent, dsi_link->name);
 
 	return 0;
 
