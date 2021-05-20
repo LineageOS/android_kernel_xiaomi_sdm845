b8077eb36ae9ed4ff59c452dcb45743678be8ea8
Jason A. Donenfeld <Jason@zx2c4.com>
2021-05-20 14:36:41 +0200

    allowedips: allocate nodes in kmem_cache
    The previous commit moved from O(n) to O(1) for removal, but in the
    process introduced an additional pointer member to a struct that
    increased the size from 60 to 68 bytes, putting nodes in the 128-byte
    slab. With deployed systems having as many as 2 million nodes, this
    represents a significant doubling in memory usage (128 MiB -> 256 MiB).
    Fix this by using our own kmem_cache, that's sized exactly right. This
    also makes wireguard's memory usage more transparent in tools like
    slabtop and /proc/slabinfo.
    
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Suggested-by: Matthew Wilcox <willy@infradead.org>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Change-Id: Iabf9dabe6a6a36aceff23e3021d51c079c9525e4

--- drivers/net/wireguard/main.c
+++ drivers/net/wireguard/main.c
@@ -27,10 +27,15 @@ static int __init mod_init(void)
 	    (ret = curve25519_mod_init()))
 		return ret;
 
+	ret = wg_allowedips_slab_init();
+	if (ret < 0)
+		goto err_allowedips;
+
 #ifdef DEBUG
+	ret = -ENOTRECOVERABLE;
 	if (!wg_allowedips_selftest() || !wg_packet_counter_selftest() ||
 	    !wg_ratelimiter_selftest())
-		return -ENOTRECOVERABLE;
+		goto err_peer;
 #endif
 	wg_noise_init();
 
@@ -56,6 +61,8 @@ err_netlink:
 err_device:
 	wg_peer_uninit();
 err_peer:
+	wg_allowedips_slab_uninit();
+err_allowedips:
 	return ret;
 }
 
@@ -64,6 +71,7 @@ static void __exit mod_exit(void)
 	wg_genetlink_uninit();
 	wg_device_uninit();
 	wg_peer_uninit();
+	wg_allowedips_slab_uninit();
 }
 
 module_init(mod_init);
