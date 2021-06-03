3c14415fdb1674053a953729f422f9ce5d552872
Chenyang Zhong <zhongcy95@gmail.com>
2021-06-03 07:23:14 -0400

    techpack: audio: fix the boundary check on asfpacketlength
    The comparison always evaluates to true, which obviously is not
    intended to happen. Fix it by changing the logic from OR to AND.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I230eb5b6bb2b2b90106240c0191ed6033d348752

--- techpack/audio/dsp/codecs/audio_wmapro.c
+++ techpack/audio/dsp/codecs/audio_wmapro.c
@@ -88,7 +88,7 @@ static long audio_ioctl_shared(struct file *file, unsigned int cmd,
 		}
 		wmapro_cfg.avg_bytes_per_sec =
 				wmapro_config->avgbytespersecond;
-		if ((wmapro_config->asfpacketlength <= 13376) ||
+		if ((wmapro_config->asfpacketlength <= 13376) &&
 		(wmapro_config->asfpacketlength > 0)) {
 			wmapro_cfg.block_align =
 				wmapro_config->asfpacketlength;
