09fb517e238c562d8ce0c9f89a526d1ac4201a8a
Bruno Martins <bgcngm@gmail.com>
2022-05-21 15:40:59 +0100

    drivers: fts: Enable FOD sensing only when screen is not on
    FOD sensing in the touchscreen driver will be only useful
    while dozing.
    
    Change-Id: I7bea97c3d390e9b1222bbf6ca2c2afc084a70e35

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -4215,12 +4215,13 @@ static int fts_drm_state_chg_callback(struct notifier_block *nb,
 				return NOTIFY_OK;
 
 			logError(1, "%s %s: FB_BLANK_POWERDOWN\n", tag, __func__);
+			info->fod_status = true;
 			queue_work(info->event_wq, &info->suspend_work);
 		} else if (val == DRM_EVENT_BLANK && blank == DRM_BLANK_UNBLANK) {
 			if (!info->sensor_sleep)
 				return NOTIFY_OK;
 			logError(1, "%s %s: FB_BLANK_UNBLANK\n", tag, __func__);
-
+			info->fod_status = false;
 			queue_work(info->event_wq, &info->resume_work);
 		}
 	}
