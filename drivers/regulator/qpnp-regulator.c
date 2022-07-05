fb7dd3f77e27efee2fecf831e5803865373bc547
Chenyang Zhong <zhongcy95@gmail.com>
2022-07-05 10:21:44 -0400

    drivers: fix section mismatch in qcom's regulator drivers
    Section mismatch was warned after upstream commit 1b9980fb24f6
    finally fixed modpost's ability to detect EXPORT_SYMBOL plus __init
    or __exit. Qcom's qpnp and stub regulator drivers have both __init
    and EXPORT_SYMBOL.
    
    - This particular 4.9 kernel does not build anything into modules.
    - Currently I do not see any code using qpnp_regulator_init or
      regulator_stub_init outside its own translation unit.
    
    Therefore, similar to d336e9a71eed, just unexport the symbol.
    
    Also, according to the kernel-hacking doc [1], "it makes no sense
    for a function marked with __init to be exported to modules with
    EXPORT_SYMBOL() or EXPORT_SYMBOL_GPL()" because functions marked
    with __init are dropped after boot is complete.
    
    [1] https://docs.kernel.org/kernel-hacking/hacking.html#init-exit-initdata
    
    Change-Id: I6a70100a07bd9dc4c6b03cd8ae76c957055491d2
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- drivers/regulator/qpnp-regulator.c
+++ drivers/regulator/qpnp-regulator.c
@@ -2423,7 +2423,6 @@ int __init qpnp_regulator_init(void)
 
 	return platform_driver_register(&qpnp_regulator_driver);
 }
-EXPORT_SYMBOL(qpnp_regulator_init);
 
 static void __exit qpnp_regulator_exit(void)
 {
