7121b20c0bec023d15e67c9659f41d24c08fb4ab
Michael Bestas <mkbestas@lineageos.org>
2023-03-01 21:13:57 +0200

    Revert "add toggle for disabling newly added USB devices"
    This has been replaced by the dwc3 usb_data_enabled toggle.
    
    This reverts commit 937de26799b194258335c3dc9772d8ad7d77c49a.
    
    Change-Id: Iccef7773009b72b69058af363eb2035d73469aac

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -98,10 +98,6 @@
 #if defined(CONFIG_SYSCTL)
 
 /* External variables not in a header file. */
-#ifdef CONFIG_USB
-int deny_new_usb __read_mostly = 0;
-EXPORT_SYMBOL(deny_new_usb);
-#endif
 extern int suid_dumpable;
 #ifdef CONFIG_COREDUMP
 extern int core_uses_pid;
@@ -1006,17 +1002,6 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec_minmax_sysadmin,
 		.extra1		= &zero,
 		.extra2		= &two,
-	},
-#endif
-#ifdef CONFIG_USB
-	{
-		.procname	= "deny_new_usb",
-		.data		= &deny_new_usb,
-		.maxlen		= sizeof(int),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax_sysadmin,
-		.extra1		= &zero,
-		.extra2		= &one,
 	},
 #endif
 	{
