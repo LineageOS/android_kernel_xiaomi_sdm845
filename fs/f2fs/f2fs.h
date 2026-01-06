1039d71bb08feb364ec23394fe297570b97cd5c6
awkoo <awkooox@gmail.com>
2026-01-07 00:20:17 +0800

    lib, zram: Merged branch for/lineage-22.2/zram from https://github.com/awkoo/kernel_mi845_lineage.git
    arm64/configs: Enabled ZRAM_ENTROPY
    arm64/configs: Enabled ZRAM_WRITEBACK
    zram: experimental patch with entropy calculation
    zram: do not waste zram_table_entry flags bits
    zram: Convert to using memset_l
    zram: Convert to hotplug state machine
    zram: pass down the bvec we need to read into in the work struct
    zram_drv: allow reclaim on bio_alloc
    zram: replace fsync_bdev with sync_blockdev
    zram: fix race between zram_reset_device() and disksize_store()
    zram: Move default compression algorithm choice to Kconfig
    zram: fix idle/writeback string compare
    lib/decompressors: remove set but not used variabled 'level'
    lib/lzo: add unlikely hints to overrun checks
    crypto: lzo - Fix compression buffer overrun
    lib/lzo/lzo1x_compress.c: replace ternary operator with min() and min_t()
    lib/lzo/lzo1x_compress.c: make lzogeneric1x_1_compress() static
    lib: 842: Improve error handling in sw842_compress()
    lib: 842: no need to check return value of debugfs_create functions
    lib/lzo/lzo1x_compress.c: fix alignment bug in lzo-rle
    lib/lzo: fix ambiguous encoding bug in lzo-rle
    lib/lzo: fix bugs for very short or empty input
    lib/lzo: separate lzo-rle from lzo
    lib/lzo: implement run-length encoding
    lib/lzo: fast 8-byte copy on arm64
    lib/lzo: 64-bit CTZ on arm64
    lib/lzo: tidy-up ifdefs
    lib/decompress_unlz4: change module to work with new LZ4 module version
    lib: decompress_unzstd: Limit output size
    lib: Add zstd support to decompress
    lib: Prepare zstd for preboot environment, improve performance
    lib/zstd/mem.h: replace __inline by inline
    lib: zstd: Mark expected switch fall-throughs
    crypto: scomp - Remove tfm argument from alloc/free_ctx
    include/linux/lz4.h: add some missing macros
    lib: lz4hc: export LZ4_resetStreamHC symbol
    lib: make LZ4_decompress_safe_forceExtDict() static
    lz4_decompress: declare LZ4_decompress_safe_withPrefix64k static
    lz4: fix LZ4_decompress_safe_partial read out of bound
    lib/lz4: explicitly support in-place decompression
    lz4: fix kernel decompression speed
    lib/lz4/lz4_decompress.c: document deliberate use of `&'
    lz4: do not export static symbol
    include/linux/lz4.h: fix spelling and copy-paste errors in documentation
    lib/lz4: update LZ4 decompressor module
    lib/lz4: make arrays static const, reduces object code size
    crypto: lz4 - fixed decompress function to return error code
    crypto: change LZ4 modules to work with new LZ4 module version
    lib/lz4: remove back-compat wrappers
    lib: update LZ4 compressor module
    crypto: acomp - update testmgr with support for acomp
    crypto: acomp - add support for deflate via scomp
    crypto: acomp - add support for 842 via scomp
    crypto: acomp - add support for lz4hc via scomp
    crypto: acomp - add support for lz4 via scomp
    crypto: acomp - add support for lzo via scomp
    crypto: acomp - add driver-side scomp interface
    crypto: acomp - add asynchronous compression api
    f2fs: Fixed kv[mz]alloc naming conflict
    mm: support __GFP_REPEAT in kvmalloc_node for >32kB
    mm, vmalloc: properly track vmalloc users
    mm: introduce kv[mz]alloc helpers
    
    Change-Id: I4c28f3c257e54a7aef2694813ac97c2f10f10bb9
    Signed-off-by: awkoo <awkooox@gmail.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -2754,16 +2754,6 @@ static inline bool f2fs_may_extent_tree(struct inode *inode)
 	return S_ISREG(inode->i_mode);
 }
 
-static inline void *kvmalloc(size_t size, gfp_t flags)
-{
-	void *ret;
-
-	ret = kmalloc(size, flags | __GFP_NOWARN);
-	if (!ret)
-		ret = __vmalloc(size, flags, PAGE_KERNEL);
-	return ret;
-}
-
 static inline void *f2fs_kmalloc(struct f2fs_sb_info *sbi,
 					size_t size, gfp_t flags)
 {
@@ -2779,16 +2769,6 @@ static inline void *f2fs_kmalloc(struct f2fs_sb_info *sbi,
 		return ret;
 
 	return kvmalloc(size, flags);
-}
-
-static inline void *kvzalloc(size_t size, gfp_t flags)
-{
-	void *ret;
-
-	ret = kzalloc(size, flags | __GFP_NOWARN);
-	if (!ret)
-		ret = __vmalloc(size, flags | __GFP_ZERO, PAGE_KERNEL);
-	return ret;
 }
 
 static inline int wbc_to_write_flags(struct writeback_control *wbc)
